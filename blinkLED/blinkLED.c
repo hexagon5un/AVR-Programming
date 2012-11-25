bff4a37f4ad0e5d89228f3048109c26c92d4cd1b
Elliot Williams <elliot@pre.postero.us>
2012-11-25 22:57:59 +0100

    blinkLED back to bit-wise naive definition for expository purposes cylonEyes to bit-shift version for same reasons

--- blinkLED/blinkLED.c
+++ blinkLED/blinkLED.c
@@ -8,21 +8,21 @@
 int main(void){
 
   // -------- Inits --------- //
-  DDRB = (1 << PB0);		/* Data Direction Register B:
+  DDRB = 0b00000001;		/* Data Direction Register B:
 				   writing a one to the bit 
 				   enables output. */
 
   // ------ Event loop ------ //
   while(1){			
 
-    PORTB = (1 << PB0);		/* Turn on the LED bit/pin in PORTB */
-    _delay_ms(200);		/* wait */
+    PORTB = 0b00000001;	      /* Turn on first LED bit/pin in PORTB */
+    _delay_ms(200);	      /* wait */
 
-    PORTB = 0;			/* Turn off all B pins, including LED */
-    _delay_ms(200);		/* wait */
+    PORTB = 0b00000000;	      /* Turn off all B pins, including LED */
+    _delay_ms(200);	      /* wait */
 
   } /* End event loop */
-  return(0);			/* This line is never reached  */
+  return(0);		      /* This line is never reached  */
 }
 
 
--- cylons/cylonEyes.c
+++ cylons/cylonEyes.c
@@ -1,43 +1,37 @@
+/* Cylon Eyes */
+
+// ------- Preamble -------- //
 #include <avr/io.h>		/* Defines pins, ports, etc */
-#define F_CPU 1000000UL	        /* Sets up the chip speed for delay.h */
 #include <util/delay.h>		/* Functions to waste time */
 
-#define DELAYTIME 500
+#define DELAYTIME 85		/* milliseconds */
+#define LED_PORT                PORTB
+#define LED_PIN                 PINB
+#define LED_DDR                 DDRB
 
-void main(void){
+int main(void){
 
-  DDRB = 0b11111111;		      /* Data Direction Register B:
-					 all set up for output */
+  // -------- Inits --------- //
+  uint8_t i;
+  LED_DDR = 0xff;		/* Data Direction Register B:
+				    all set up for output */ 
+  
+  // ------ Event loop ------ //
+  while(1){			
 
-  while(1){
-    PORTB = 0b00000001;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b00000010;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b00000100;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b00001000;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b00010000;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b00100000;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b01000000;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b10000000;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b01000000;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b00100000;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b00010000;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b00001000;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b00000100;
-    _delay_ms(DELAYTIME);
-    PORTB = 0b00000010;
-    _delay_ms(DELAYTIME);
-  }  
+    while(i < 7){
+       LED_PORT = (1 << i);	/* illuminate only i'th pin */
+      _delay_ms(DELAYTIME);	/* wait */
+      i = i + 1;		/* move to the next LED */
+    }
+
+    while(i > 0){
+      LED_PORT = (1 << i);	/* illuminate only i'th pin */
+      _delay_ms(DELAYTIME);	/* wait */
+      i = i - 1;		/* move to the previous LED */
+    }
+
+  } /* End event loop */
+  return(0);		     
 }
 
