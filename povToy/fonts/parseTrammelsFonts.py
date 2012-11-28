7ac6e3ed0c7d0e2db179080354a8881d40095e9d
Elliot Williams <elliot@pre.postero.us>
2012-11-28 11:13:08 +0100

    Crap.  In an attempt to get fonts all wrangled, discovered that they are max 11 pixels high instead of 8.

--- povToy/fonts/parseTrammelsFonts.py
+++ povToy/fonts/parseTrammelsFonts.py
@@ -4,60 +4,67 @@
 FNAME = "font-small.in"
 rawFont = open(FNAME)
 
-letters = {}
-letterBytes = None
-currentLetter = None
-
-def columnsToBytes(letter):
+def __stripLetter(letterText):
+    ## This just takes the fonts out of the format I got them in
     # all padded with one extra space to left, two on top or bottom
-    letter = letter[2:-3]
-    letter = [row[1:6] for row in letter]
+    letterText = letterText[2:-3]
     # make 5 binary bytes columnwise
-    letterBytes = []
-    for column in range(0,5):
-        columnByte = 0
-        for bit in range(0,8):
-            if letter[bit][column] == "#":
-                columnByte += 2**bit
-        letterBytes.append(columnByte)
+    letterText = [row[1:6] for row in letterText]
+    return(letterText)
+
+def rawLetterToBinary(letterText):
+    '''Convert to column-wise binary'''
+    letterBytes = ['0b'] * len(letterText[0])
+    for row in letterText:
+        for columnNum, cell in enumerate(row):
+            if cell == "#":
+                letterBytes[columnNum] += "1"
+            else:
+                letterBytes[columnNum] += "0"
+    ## Flip upside-down for legibility in .h file
+    letterBytes.reverse()
     return(letterBytes)
-
-
-for line in rawFont:
-    if line.strip().endswith("="):
-        # Save out whatever we've got so far
-        if currentLetter:
-            letters[currentLetter] = letterBytes
-
-        # And on to the next one
-        currentLetter = line[0] 
-        letters[currentLetter] = []
-        letterBytes = []
-    else:
-        letterBytes.append(line)
-
-letters.pop("~")
-encodedBytes = dict([[letter, columnsToBytes(letters[letter])] for letter in letters])
-
+ 
 def writeFontHeader(fileName, whichLetters, encodedBytes):
-    whichLetters.sort()
     font = open(fileName + ".h", "write")
-    font.write("// A font alphabet in 5x8\n \n\n\n")
+    font.write("// A font alphabet  \n\n\n")
     font.write("const uint8_t ")
     font.write(fileName)
     font.write("[{}][{}] = ".format(len(whichLetters), len(encodedBytes[whichLetters[0]])))
     font.write("{\n")
     for letter in whichLetters:
         font.write("\t//  {}  -- ASCII {}\n".format(letter, ord(letter)))
-        font.write('\t{')
-        font.write("{x[0]}, {x[1]}, {x[2]}, {x[3]}, {x[4]}".format(x=encodedBytes[letter]))
+        font.write('{ ')
+        for byte in encodedBytes[letter]:
+            font.write("{},\n  ".format(byte))
         font.write("},\n")
     font.write("};\n\n")
     font.close()
 
 
-writeFontHeader("thinFont", encodedBytes.keys(), encodedBytes)
+if __name__ == "__main__":
 
-import string
-writeFontHeader("uppercase", list(string.letters[26:]), encodedBytes)
-writeFontHeader("lowercase", list(string.letters[:26]), encodedBytes)
+    ## Read in file letter by letter
+    rawLetters = {}
+    for line in rawFont:
+        if "=" in line:             # character definition
+            currentLetter = line[0]
+            rawLetters[currentLetter] = []
+        else:
+            rawLetters[currentLetter].append(line.strip("\n"))
+
+    ## Strip out extra padding, empty characters
+    for letter in rawLetters:
+        rawLetters[letter] = __stripLetter(rawLetters[letter])
+        
+    ## Convert text format to binary representation
+    encodedLetters = {}
+    for letter in rawLetters:
+        encodedLetters[letter] = rawLetterToBinary(rawLetters[letter])
+
+    ## Sorting puts them in ASCII order
+    writeFontHeader("thinFont", sorted(encodedLetters.keys()), encodedLetters)
+
+    import string
+    writeFontHeader("uppercase", sorted(list(string.letters[26:])), encodedLetters)
+    writeFontHeader("lowercase", sorted(list(string.letters[:26])), encodedLetters)
