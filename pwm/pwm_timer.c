677e8db31787d7f5d3a1c6b54f2e563467e7cded
Elliot Williams <elliot@pre.postero.us>
2012-12-13 18:17:11 +0100

    got all three PWM channels on the LEDs running, serial interface.

--- pwm/pwm_timer.c
+++ pwm/pwm_timer.c
@@ -11,11 +11,22 @@ Quick and dirty PWM Demo
 #include "macros.h"
 #include "USART.h"
 
-static inline void initTimer2(void){
+static inline void initTimers(void){
+  // Timer 1 A,B
+  set_bit(TCCR1A, WGM10);       /* Fast PWM mode, 8-bit */
+  set_bit(TCCR1B, WGM12);       /* Fast PWM mode, pt.2 */
+  set_bit(TCCR1B, CS11);        /* F_CPU/8 -- 1MHz with fast fuses */  
+
+  set_bit(TCCR1A, COM1A1);      /* PWM output on OCR1A */
+  set_bit(TCCR1A, COM1B1);      /* PWM output on OCR1B */
+  set_bit(LED_DDR, LED1);       /* enable output on pin */
+  set_bit(LED_DDR, LED2);       /* enable output on pin */
+
+  // Timer 2
   set_bit(TCCR2A, WGM20);       /* Fast PWM mode */
   set_bit(TCCR2A, WGM21);       /* Fast PWM mode, pt.2 */
-  
   set_bit(TCCR2B, CS21);        /* F_CPU/8 -- 1MHz with fast fuses */
+
   set_bit(TCCR2A, COM2A1);      /* PWM output on OCR2A */
   set_bit(LED_DDR, LED3);       /* enable output on pin */
 }
@@ -26,6 +37,17 @@ static inline void echo(uint8_t brightness){
   transmitString("\r\n");
 }
 
+static inline uint8_t generate8BitBrightness(uint8_t brightness){
+  if ((brightness >= '0') && (brightness <= '9')){
+      brightness = (brightness - '0') * 28;
+      return(brightness);
+  }
+  else {
+    return(0);
+  }
+}
+
+
 
 int main(void){
   
@@ -33,22 +55,20 @@ int main(void){
 
   // -------- Inits --------- //
 
-  initTimer2();
-  OCR2A = 0;
+  initTimers();
 
   initUSART();
   _delay_ms(1000);
-  transmitString("Type a number 0-9\r\n");
+  transmitString("Type numbers 0-9\r\n");
   
   // ------ Event loop ------ //
   while(1){	
     
     brightness = receiveByte();
-    if ((brightness >= '0') && (brightness <= '9')){
-      OCR2A = (brightness - '0') * 28;
-      echo(brightness);
-    }
-    
+    echo(brightness);
+    OCR2A = OCR1B;
+    OCR1B = OCR1A;
+    OCR1A = generate8BitBrightness(brightness);
 
   } /* End event loop */
   return(0);		      /* This line is never reached  */
