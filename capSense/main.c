e1b9a45fdd524c668c446973f23acfeb5d5434b6
Elliot Williams <elliot@pre.postero.us>
2012-12-28 16:33:53 +0100

    setup for capSense project with main.c/main.h style

---
+++ capSense/main.c
@@ -0,0 +1,20 @@
+/*  */
+
+// ------- Preamble -------- //
+#include "main.h"
+
+int main(void){
+
+  // -------- Inits --------- //
+
+  // ------ Event loop ------ //
+  while(1){			
+
+  } /* End event loop */
+  return(0);		      /* This line is never reached  */
+}
+
+
+
+
+
---
+++ capSense/main.h
@@ -0,0 +1,14 @@
+
+
+
+/* Libraries */
+#include <avr/io.h>             
+#include <util/delay.h>         
+#include "pinDefines.h"
+#include "macros.h"
+
+/* Global Variables */
+
+
+/* Function Prototypes */
+
