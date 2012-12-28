96d636b6e7a341805f7c194838675cf58fe89992
Elliot Williams <elliot@pre.postero.us>
2012-12-28 21:14:19 +0100

    Naive (charge-time) cap sensor routine

--- capSense/main.h
+++ capSense/main.h
@@ -1,14 +1,22 @@
-
-
-
-/* Libraries */
+/* Includes */
 #include <avr/io.h>             
 #include <util/delay.h>         
+#include <avr/interrupt.h>
+
 #include "pinDefines.h"
 #include "macros.h"
+#include "USART.h"
+
+/* Defines */
+#define THRESHOLD 10		/* how long to count as a press */
+/* note, shouldn't be below 8, it needs that long just for the code */
+/* but unless you're using a big antenna (lots of metal) near 8 is good */
 
 /* Global Variables */
 
 
 /* Function Prototypes */
 
+static inline void initTimer0(void);
+
+static inline uint8_t pollSensor(void);
