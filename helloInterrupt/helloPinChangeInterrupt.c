7c8627ed16cbdc4e7d7567008cfe4e62e18f77bd
Elliot Williams <elliot@pre.postero.us>
2013-01-12 15:57:52 +0100

    cleaned up pin-change example, started up on fancy timer applications

--- helloInterrupt/helloPinChangeInterrupt.c
+++ helloInterrupt/helloPinChangeInterrupt.c
@@ -7,22 +7,19 @@
 #include "pinDefines.h"
 #include "macros.h"
 
-#define DEBOUNCE_TIME 2         /* milliseconds */
-
-ISR(INT0_vect){                 /* Run every time button state changes */
-  _delay_ms(DEBOUNCE_TIME);
-  if (bit_is_clear(BUTTON_IN, BUTTON)){
-    LED_PORT = (LED_PORT << 1) ; /* roll to next LED */
-    if (!LED_PORT){		 /* if rolled off the left end... */
-      LED_PORT = (1 << LED1);	 /* start back at LED1 */
-    }
+ISR(PCINT2_vect){                 /* Run every time button state changes */
+  if (bit_is_set(BUTTON_IN, BUTTON)){
+    set_bit(LED_PORT, LED1);
+  }
+  else{
+    clear_bit(LED_PORT, LED1);
   }
 }
 
 void initPinChangeInterrupt18(void){
-  set_bit(PCICR, PCIE2);	/* set pin-change interrupt for D pins */
-  set_bit(PCMSK2, PCINT18);	/* set mask to look for PCINT18 / PD2 */
-  sei();                       /* set (global) interrupt enable bit */
+  set_bit(PCICR, PCIE2);     /* set pin-change interrupt for D pins */
+  set_bit(PCMSK2, PCINT18);  /* set mask to look for PCINT18 / PD2 */
+  sei();		     /* set (global) interrupt enable bit */
 }
 
 int main(void){
