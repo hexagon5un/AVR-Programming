9bf161f7ec723581e5810a28eac435a98cba7f21
Elliot Williams <elliot@pre.postero.us>
2013-01-14 11:24:54 +0100

    framework of rot13, but no code

---
+++ rot13/main.h
@@ -0,0 +1,17 @@
+/* Includes */
+#include <avr/io.h>             
+#include <avr/interrupt.h>             
+#include <util/delay.h>         
+
+#include "pinDefines.h"
+#include "macros.h"
+#include "USART.h"
+
+/* Defines */
+
+
+/* Global Variables */
+
+
+/* Function Prototypes */
+
