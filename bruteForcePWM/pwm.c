f97f875e1f4cb3c9604741dcb1319fb3abdbb3e1
Elliot Williams <elliot@pre.postero.us>
2013-01-16 15:59:04 +0100

    brute-force PWM version of code

---
+++ bruteForcePWM/pwm.c
@@ -0,0 +1,56 @@
+/*  
+
+Quick and dirty PWM Demo
+
+*/
+
+// ------- Preamble -------- //
+#include <avr/io.h>		/* Defines pins, ports, etc */
+#include <util/delay.h>		/* Functions to waste time */
+#include "pinDefines.h"
+#include "macros.h"
+
+#define LED_DELAY 20
+
+int main(void){
+  
+  uint8_t brightness = 0;
+  int8_t direction = 1;
+  uint8_t i;
+
+  // -------- Inits --------- //
+
+  // Init all LEDs
+  LED_DDR = 0xff;  
+
+  // ------ Event loop ------ //
+  while(1){	
+    
+    // PWM
+    for (i=0; i<255; i++){
+      if (i < brightness){
+	LED_PORT = 0xff;	/* turn on */
+      }
+      else{
+	LED_PORT = 0;		/* turn off */
+      }
+      _delay_us(LED_DELAY);
+    }
+
+    // Brighten and dim 
+    if (brightness == 0){
+      direction = 1;
+    }
+    if (brightness == 255){
+      direction = -1;
+    }
+    brightness += direction;
+
+
+  } /* End event loop */
+  return(0);		      /* This line is never reached  */
+}
+
+
+
+
