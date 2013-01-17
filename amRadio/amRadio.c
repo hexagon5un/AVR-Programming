d30ad8e4a3b192d9d4f3a01e6281412e7e9569bc
Elliot Williams <elliot@pre.postero.us>
2013-01-17 19:44:22 +0100

    changes made during final edit of timer, interrupt, and pwm chapters

--- amRadio/amRadio.c
+++ amRadio/amRadio.c
@@ -1,6 +1,7 @@
 /*  
 
-
+Plays a simple tune, broadcasts it in the AM radio band.
+Basically just showing off with timers.
 
 */
 
@@ -9,6 +10,7 @@
 #include <util/delay.h>		/* Functions to waste time */
 #include "pinDefines.h"
 #include "macros.h"
+#include "scale16.h"
 
 #define COUNTER_VALUE   5  	/* determines carrier frequency */
 // From f = f_cpu / ( 2* N* (1 + OCRnx) )
@@ -25,6 +27,8 @@
 // 8Mhz / (2 * 1 * (1+5)) = 670 kHz
 // 8Mhz / (2 * 1 * (1+7)) = 500 kHz
 
+#define NOTE_LENGTH  9000
+
 static inline void initTimer(void){
 
   set_bit(TCCR0A, WGM01);	/* CTC mode */
@@ -34,6 +38,24 @@ static inline void initTimer(void){
   OCR0A = COUNTER_VALUE;	/* carrier frequency */
 }
 
+static inline void transmitBeep(uint16_t pitch){
+  uint16_t elapsed;
+  uint16_t i;
+  for(elapsed = 0; elapsed < NOTE_LENGTH; elapsed += pitch){
+    for (i = 0; i < pitch; i++){ /* delay for pitch cycles */
+      asm volatile("nop"::);	
+      asm volatile("nop"::); /* eight no-operations takes 1us at 8MHz */
+      asm volatile("nop"::);	
+      asm volatile("nop"::);	
+      asm volatile("nop"::);	
+      asm volatile("nop"::);	
+      asm volatile("nop"::);	
+      asm volatile("nop"::);	
+    }    
+    toggle_bit(ANTENNA_DDR, ANTENNA);
+  }
+  set_bit(ANTENNA_DDR, ANTENNA);  /* back on full */
+}
 
 int main(void){
   
@@ -41,21 +63,29 @@ int main(void){
 
   // -------- Inits --------- //
   initTimer();
-  set_bit(BUTTON_PORT, BUTTON);	/* enable pullup on button */
   
   // ------ Event loop ------ //
   while(1){	
-
-    /* Modulate while button is pressed */
-    if (bit_is_clear(BUTTON_IN, BUTTON)) {
-      // Enable output mode: full power
-      set_bit(ANTENNA_DDR, ANTENNA);
-      _delay_us(1200);
-      
-      // Turn off output: no power
-      clear_bit(ANTENNA_DDR, ANTENNA);
-      _delay_us(1200);
-    }
+    
+    transmitBeep(E2);
+    _delay_ms(2);
+    transmitBeep(E2);
+    _delay_ms(200);
+    transmitBeep(E2);
+    _delay_ms(200);
+    transmitBeep(C2);
+    transmitBeep(E2);
+    _delay_ms(200);
+    transmitBeep(G2);
+    transmitBeep(G2);
+    _delay_ms(500);
+    transmitBeep(G1);
+    transmitBeep(G1);
+    
+    _delay_ms(300);
+    _delay_ms(300);
+    _delay_ms(300);
+    _delay_ms(300);
 
   } /* End event loop */
   return(0);		      /* This line is never reached  */
