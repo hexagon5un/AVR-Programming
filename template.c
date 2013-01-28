0e6ed1ab415f877da4768bc95b936d08ebd1a867
Elliot Williams <elliot@pre.postero.us>
2013-01-28 17:10:52 +0100

    wrote quick project-framework code b/c tired of doing it also starting up skeleton on ADC/temp-sensor code

---
+++ template.c
@@ -0,0 +1,27 @@
+/* */
+
+// ------- Preamble -------- //
+#include <avr/io.h>             
+#include <util/delay.h>         
+#include <avr/interrupt.h>
+
+#include "pinDefines.h"
+#include "macros.h"
+
+// -------- Global Variables --------- //    
+
+// -------- Functions --------- //
+
+int main(void){
+
+  // -------- Inits --------- //
+
+  // ------ Event loop ------ //
+  while(1){     
+    
+    
+    
+  }    /* End event loop */
+  return(0);                  /* This line is never reached  */
+}
+
