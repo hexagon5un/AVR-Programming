ee377e20c6b842b14162730a937fe203f9cf7e90
Elliot Williams <elliot@pre.postero.us>
2013-03-31 14:05:00 +0200

    vibration sensor code up and running

--- accelerometer/accelerometer.c
+++ accelerometer/accelerometer.c
@@ -5,15 +5,12 @@
 #include <util/delay.h>         
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
-
-
 #include "pinDefines.h"
 #include "macros.h"
 
-#define PADDING               5
-#define SENSITIVITY_SAMPLES  20
-#define NUM_BLINKS           20   
-#define ON_TICKS             500	/* in hundredths of seconds */
+#define PADDING              5
+#define CALIBRATION_SAMPLES  20
+#define ON_TICKS             250	/* 2.5 sec, in hundredths of seconds */
 
 // -------- Global Variables --------- //    
 volatile uint16_t ticks;
@@ -21,9 +18,9 @@ volatile uint16_t ticks;
 // -------- Functions --------- //
 static inline void initTicks(){
   TCCR0A |= (1 << WGM01);	/* CTC mode */
-  TCCR0B |= (1<<CS00)|(1<<CS02);	/* 1 MHz / 1024 */
-  TIMSK |= (1<<OCIE0A); 	/* output compare interrupt enable*/
-  OCR0A = 10;			/* 1 Mhz / 1024 / 100 = 102.4 ms */
+  TCCR0B |= (1<<CS00)|(1<<CS02);/* 8 MHz / 1024 */
+  TIMSK0 |= (1<<OCIE0A); 	/* output compare interrupt enable*/
+  OCR0A = 77;			/* 78 / (8MHz / 1024) =  9.984 ms*/
   sei();			/* set (global) enable interrupt bit */
   ticks=0;
 }
@@ -32,6 +29,29 @@ ISR(TIMER0_COMPA_vect){
   ticks++;
 }
 
+static inline void initADC(void){
+  ADMUX  |= (0b00001111 & PC1);  /* set mux to ADC1 */
+  ADMUX  |= (1 << REFS0);        /* reference voltage on AVCC */
+  ADCSRA |= (1 << ADPS1) | (1 << ADPS2); /* ADC clock prescaler /64 */
+  ADCSRA |= (1 << ADEN);        /* enable ADC */
+  DIDR0 |= _BV(ADC1D);	  /* disable digital input for power saving */
+}
+
+static inline void sleepDelay(uint16_t numTicks){
+  ticks = 0;	  
+  while(ticks < numTicks){
+    sleep_mode();
+  }
+}
+
+static inline void blink(uint8_t times){
+  while(times){
+    toggle_bit(LED_PORT, LED0);
+    toggle_bit(LED_PORT, LED1);
+    sleepDelay(20);
+    times--;
+  }
+}
 
 int main(void){
 
@@ -41,33 +61,30 @@ int main(void){
   uint16_t adcValue;
   uint16_t maxValue;
   uint16_t minValue;
+  
   // Initializations here
+  set_sleep_mode(SLEEP_MODE_IDLE);
+  
   /* 2 LEDs as output, pos and neg excursions */
-  LED_DDR |= (1 << LED1) | (1<< LED2);
-  LED_PORT |= (1<<LED1) | (1<<LED2); /* Both off */
-  SWITCH_DDR |= (1 << SWITCH);	     /* turns light on */
+  set_bit(LED_DDR, LED0);
+  set_bit(LED_DDR, LED1);
+  set_bit(LED_DDR, LED7);	     /* active switch */
+
+  initADC();
 
   /* Setup system timing and wait a bit */
   initTicks();
-  ticks = 0;		       
-  while(ticks < ON_TICKS){;}
+  sleepDelay(100); 		/* let all settle for 1 sec after reset */
+  set_bit(LED_PORT, LED0);
 
-  /* Init ADC */
-  ADMUX = 1;			/*  ADC1 */
-  ADCSRA |= _BV(ADEN) | _BV(ADSC) | _BV(ADPS0) | _BV(ADPS1); /* prescale 8, start up */
-  DIDR0 |= _BV(ADC1D);					     /* disable digital input for power saving */
-
-  /* Auto-sensitivity routine */
+  /* Auto-sensitivity calibration routine */
   maxValue = 0;			/* start with small max, big min */
   minValue = 1023;
-  LED_PORT ^= (1 << LED1);
-  for (i = 0; i < SENSITIVITY_SAMPLES; i++){
-    /* blink to let you know it's happening */
-    LED_PORT ^= (1 << LED1);
-    LED_PORT ^= (1 << LED2);
-    _delay_ms(200);
+  for (i = 0; i < CALIBRATION_SAMPLES; i++){
+    /* blink alternately = calibrating */
+    blink(1);
     /* sample */
-    ADCSRA |= _BV(ADSC);		
+    set_bit(ADCSRA, ADSC);		
     loop_until_bit_is_clear(ADCSRA, ADSC);
     adcValue = ADC;
     /* update max/min */
@@ -78,48 +95,42 @@ int main(void){
       minValue = adcValue;
     }
   }
-  /* pad values to allow for deadband */
-  maxValue += PADDING;
-  minValue -= PADDING;
-  
-  /* End-of-calibration lightshow */
-  LED_PORT |= (1<<LED1) | (1<<LED2); /* Both off */
-  for (i=0; i < 8; i++){
-    LED_PORT ^= (1 << LED1);
-    LED_PORT ^= (1 << LED2);
-    _delay_ms(100);
-  }
+
+  /* Blink both to signal done */
+  clear_bit(LED_PORT, LED0);
+  clear_bit(LED_PORT, LED1);
+  blink(10);
 
   // ------ Event loop ------ //
   while(1){     
 
     /* Do conversion */
-    ADCSRA |= _BV(ADSC);		
+    set_bit(ADCSRA, ADSC);
     loop_until_bit_is_clear(ADCSRA, ADSC);
     adcValue = ADC;
     
     /* Light up if outside threshold */
-    if (adcValue < minValue){
-      LED_PORT &= ~(1 << LED1);	/* one on */
-      SWITCH_PORT |= (1 << SWITCH); /* turn switch on */
+    if (adcValue < (minValue-PADDING)){
+      set_bit(LED_PORT, LED0);	/* one on */
+      set_bit(LED_PORT, LED7);  /* turn switch on */
       lightsOutTime = ticks + ON_TICKS;
     }
-    else if (adcValue > maxValue){
-      LED_PORT &= ~(1 << LED2); /* two on */
-      SWITCH_PORT |= (1 << SWITCH); /* turn switch on */
+    else if (adcValue > (maxValue+PADDING)){
+      set_bit(LED_PORT, LED1); /* two on */
+      set_bit(LED_PORT, LED7); /* turn switch on */
       lightsOutTime = ticks + ON_TICKS;
     }
     else{
       /* Nothing seen, turn off light when it's time */
-      LED_PORT |= (1<<LED1) | (1<<LED2); /* Both off */
+      clear_bit(LED_PORT, LED0);
+      clear_bit(LED_PORT, LED1); /* Both off */
       if (ticks == lightsOutTime){
-	SWITCH_PORT &= ~(1 << SWITCH); /* turn switch off */
-	_delay_ms(100);		       /* there's some glitch when power back on */
+	clear_bit(LED_PORT, LED7); /* turn switch off */
       }
     }
 
-    
-    
+    sleep_mode();		/* sleep for the rest of the tick */
+
   }    /* End event loop */
   return(0);                  /* This line is never reached  */
 }
