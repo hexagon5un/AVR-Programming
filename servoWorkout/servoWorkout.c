bf5cb85f1bef63d284b4ed585c3942dd26f060c9
Elliot Williams <elliot@pre.postero.us>
2013-04-17 21:37:45 +0200

    finished version of servo workout

--- servoWorkout/servoWorkout.c
+++ servoWorkout/servoWorkout.c
@@ -1,4 +1,4 @@
-/* */
+/* Quick interactive demo running servo with Timer 1 */
 
 // ------- Preamble -------- //
 #include <avr/io.h>             
@@ -9,8 +9,67 @@
 #include "macros.h"
 #include "USART.h"
 
-#define NUM_SERVO_PULSES     60 /* how many repetitions to send */
-#define BETWEEN_PULSE_DELAY  20 	/* ms */
+#define PULSE_MIN          400	/* experiment with these values */
+#define PULSE_MAX         2250	/* to match your own servo */
+#define PULSE_MID         1325
+
+static inline uint16_t getNumber(void);
+
+static inline void initTimer1Servo(void){
+  /* Set up Timer1 (16bit) to give a pulse every 20ms*/
+  set_bit(TCCR1A, WGM11); /* Using Fast PWM mode */
+  set_bit(TCCR1B, WGM12); /* counter max in ICR1 */
+  set_bit(TCCR1B, WGM13); 
+ 
+  set_bit(TCCR1B, CS11);        /* /8 prescaling -- counting in microseconds*/
+  ICR1 = 20000;                 /* TOP value = 20ms */
+
+  set_bit(TCCR1A, COM1A1);	/* Direct output on PB1 / OC1A */
+  set_bit(DDRB, PB1);		/* set pin direction to output */
+}
+
+static inline void showOff(void){
+  transmitString("Center\r\n");
+  OCR1A = PULSE_MID;
+  _delay_ms(1500);
+  transmitString("Clockwise Max\r\n");
+  OCR1A = PULSE_MIN;
+  _delay_ms(1500);
+  transmitString("Counterclockwise Max\r\n");
+  OCR1A = PULSE_MAX;
+  _delay_ms(1500);
+  transmitString("Center\r\n");  
+  OCR1A = PULSE_MID;
+  _delay_ms(1500);
+}
+
+int main(void){
+
+  // -------- Inits --------- //
+  uint16_t servoPulseLength;
+  OCR1A = PULSE_MID;  		/* set it to middle position initially */
+  initTimer1Servo();                 
+  initUSART();
+  transmitString("\r\nWelcome to the Servo Demo\r\n");
+  showOff();
+ 
+  // ------ Event loop ------ //
+  while(1){     
+
+    transmitString("\r\nEnter a four-digit pulse length:\r\n");
+    servoPulseLength = getNumber();
+
+    transmitString("On my way....\r\n");
+    OCR1A = servoPulseLength;
+    set_bit(DDRB, PB1);		/* re-enable output pin */
+
+    _delay_ms(2000);
+    transmitString("Releasing...\r\n");
+    clear_bit(DDRB, PB1);	/* disable output pin */
+
+  }    /* End event loop */
+  return(0);                  /* This line is never reached  */
+}
 
 static inline uint16_t getNumber(void){
   // Gets a PWM value from the serial port.
@@ -33,44 +92,3 @@ static inline uint16_t getNumber(void){
   transmitByte('\n');           /* newline */
   return(1000*(thousands -'0') + 100*(hundreds-'0') + 10*(tens-'0') + ones-'0');
 }
-
-static inline void moveTo(uint16_t servoPulseLength){
-   uint8_t i;
-   uint16_t j;
-
-   for (i=0; i<NUM_SERVO_PULSES; i++){
-     set_bit(PORTB, PB1);
-     j=0;
-     while(j < servoPulseLength){
-       _delay_us(10);
-       j += 10;
-     }
-     clear_bit(PORTB, PB1);
-     _delay_ms(BETWEEN_PULSE_DELAY);
-   }
-}
-
-int main(void){
-
-  // -------- Inits --------- //
-  uint8_t i;
-  uint16_t servoPulseLength;
-  DDRB |= BV(PB1);
-  
-  initUSART();
-  transmitString("\r\nWelcome to the Servo Tester\r\n");
-  transmitString("Centering...\r\n");
-  moveTo(1500);
-
-  // ------ Event loop ------ //
-  while(1){     
-
-    transmitString("\r\nEnter a four-digit pulse length:\r\n");
-    servoPulseLength = getNumber();
-    transmitString("On my way....\r\n");
-    moveTo(servoPulseLength);
-   
-  }    /* End event loop */
-  return(0);                  /* This line is never reached  */
-}
-
