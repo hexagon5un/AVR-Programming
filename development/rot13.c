d9d3fc767fd1b76d5a2d40bedcf47cb710bdc8b5
Elliot Williams <elliot@pre.postero.us>
2013-04-17 21:40:18 +0200

    rot13 code.  Will I ever use this?

---
+++ development/rot13.c
@@ -0,0 +1,33 @@
+/* */
+
+// ------- Preamble -------- //
+#include <avr/io.h>             
+#include <util/delay.h>         
+#include <avr/interrupt.h>
+
+#include "pinDefines.h"
+#include "macros.h"
+#include "USART.h"
+
+// -------- Global Variables --------- //    
+
+// -------- Functions --------- //
+
+int main(void){
+
+  uint8_t a;
+
+  // -------- Inits --------- //
+  initUSART();
+  sayOK();  
+
+  // ------ Event loop ------ //
+  while(1){     
+
+    
+    
+    
+  }    /* End event loop */
+  return(0);                  /* This line is never reached  */
+}
+
