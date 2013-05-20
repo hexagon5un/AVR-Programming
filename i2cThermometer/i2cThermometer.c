1e2099c801d7c42bfb4a9e0f95890ab3fee0d9fb
Elliot Williams <elliot@pre.postero.us>
2013-05-20 21:28:03 +0200

    split off i2c stuff into own library still depends on some of the pin defines -- need to factor that out eventually

--- i2cThermometer/i2cThermometer.c
+++ i2cThermometer/i2cThermometer.c
@@ -3,57 +3,18 @@
 // ------- Preamble -------- //
 #include <avr/io.h>             
 #include <util/delay.h>         
-#include <avr/interrupt.h>
 
 #include "pinDefines.h"
 #include "macros.h"
 #include "USART.h"
+#include "i2c.h"
 
-// -------- Global Variables --------- //    
+// -------- Defines -------- //    
 
 #define LM75_ADDRESS_W 0b10010000
 #define LM75_ADDRESS_R 0b10010001
 
 // -------- Functions --------- //
-
-static inline void initI2C(void){
-  set_bit(I2C_SDA_PORT, I2C_SDA); /* pullup for SDA line */
-  set_bit(I2C_SCL_PORT, I2C_SCL); /* pullup for SCL line */
-  TWBR = 32;			  /* set bit rate, see p. 242 */
-			    /* 8MHz / (16+2*TWBR*1) ~= 100kHz */
-  TWCR |= (1 << TWEN);		 /* enable */
-}
-
-static inline void i2cWaitForComplete(void){
-  loop_until_bit_is_set(TWCR, TWINT);
-}
-
-static inline void i2cStart(void){
-  TWCR = (_BV(TWINT)|_BV(TWEN)|_BV(TWSTA));
-  i2cWaitForComplete();
-}
-
-static inline void i2cStop(void){
-  TWCR = (_BV(TWINT)|_BV(TWEN)|_BV(TWSTO));
-}
-
-static inline uint8_t i2cReadAck(void){
-  TWCR = (_BV(TWINT)|_BV(TWEN)|_BV(TWEA)); 
-  i2cWaitForComplete();
-  return(TWDR);
-}
-
-static inline uint8_t i2cReadNoAck(void){
-  TWCR = (_BV(TWINT)|_BV(TWEN)); 
-  i2cWaitForComplete();
-  return(TWDR);
-}
-
-static inline void i2cSend(uint8_t data){
-  TWDR = data;
-  TWCR = (_BV(TWINT)|_BV(TWEN)); /* init and enable */
-  i2cWaitForComplete();
-}
 
 int main(void){
 
