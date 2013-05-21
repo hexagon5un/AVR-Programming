c2ea3f4942b6d3eda98fbcc6a7ca7d725d434801
Elliot Williams <elliot@pre.postero.us>
2013-05-21 21:15:20 +0200

    wholesale re-work of the spi EEPROM demo. now it's an interactive thingy where you type into memory doesn't demo the full library, but whatevers.

--- spiEEPROMDemo/spiEEPROMDemo.c
+++ spiEEPROMDemo/spiEEPROMDemo.c
@@ -9,55 +9,46 @@
 #include "USART.h"
 #include "25LC256.h"
 
-#define ARBITRARY_MEMORY_LOCATION  123
-
 int main(void){
-  uint16_t testNumber;
+  uint8_t i;
+  uint8_t address;
 
   // -------- Inits --------- //
   initSPI();
   initUSART();
-  printString("\r\n====  EEPROM Memory Test ====\r\n");
-
-  printString("This was in memory on reset: ");
-  printByte(EEPROM_getByte(ARBITRARY_MEMORY_LOCATION));
-  printString(" ");
-  printByte(EEPROM_getByte(ARBITRARY_MEMORY_LOCATION+1));
-  printString("\r\n");
   
-  /* Start with a clean slate */
-  /* Be patient, this can take a few seconds */
-  set_bit(LED_DDR, LED0);
-  set_bit(LED_PORT, LED0);
-  printString("Erasing EEPROM, be patient...\r\n");
-  EEPROM_clearAll();
-  clear_bit(LED_PORT, LED0);
-
-  printString("\r\nThese bytes should all be zero... ");
-  printByte(EEPROM_getByte(0x0000));
-  printString(" ");
-  printByte(EEPROM_getByte(0x7fff));
-  printString("\r\n");
-
-  testNumber = 250;
-
   // ------ Event loop ------ //
   while(1){     
+    printString("\r\n====  EEPROM Memory Playground ====\r\n");
+    printString("Address  Value\r\n");
 
-    // Display memory contents -- should start off 000, 000
-    //  and then increase / change as we write to it
-    printString("What's in memory?  ");
-    printByte(EEPROM_getByte(ARBITRARY_MEMORY_LOCATION));
-    printString(" ");
-    printByte(EEPROM_getByte(ARBITRARY_MEMORY_LOCATION+1));
-    printString("\r\n");
-    
-    // Write a value in to memory
-    EEPROM_writeWord(ARBITRARY_MEMORY_LOCATION, testNumber);
-    // Increment it
-    testNumber++;
+    for (i=0; i<10; i++){	/* print out first ten bytes of memory */
+      printString("  ");
+      printByte(i);
+      printString("     ");
+      printByte(EEPROM_getByte(i));
+      printString("\r\n");
+    }
+    printString(" [e] to erase all memory\r\n");
+    printString(" [w] to write byte to memory\r\n\r\n");
 
-    _delay_ms(2000);
+    switch(receiveByte()){	/* take input */
+    case 'e':
+      printString("Clearing EEPROM, this could take a few seconds.\r\n");
+      EEPROM_clearAll();
+      break;
+    case 'w':
+      printString("Which memory slot would you like to write to?\r\n");
+      address = getNumber();
+      printString("\r\nWhat number would you like to store there?\r\n");
+      i = getNumber();
+      EEPROM_writeByte(address, i);
+      printString("\r\n");
+      break;
+    default:
+      printString("What??\r\n");
+    }
+
 
   }    /* End event loop */
   return(0);                  /* This line is never reached  */
