c88041a315c2831951cc58ed475412813f5682ed
Elliot Williams <elliot@pre.postero.us>
2013-05-23 18:10:24 +0200

    logging thermometer -- moved menu out of mainloop, now enters on serial entry

--- loggingThermometer/loggingThermometer.c
+++ loggingThermometer/loggingThermometer.c
@@ -23,7 +23,8 @@
 /* store seconds-delay value here */
 #define MEMORY_START              4        
 /* where to start logging temperature values */
-
+#define MENU_DELAY                5
+/* seconds to wait before bypassing main menu */
 
 // -------- Functions --------- //
 
@@ -43,7 +44,7 @@ int main(void){
   uint16_t currentMemoryLocation; /* where are we in EEPROM? */
   uint16_t i;			  /* used to count memory locations */
   uint8_t tempHighByte, tempLowByte, temperatureByte; /* from LM75 */
-  uint8_t enterMenu = 1;	/* logical flag */
+  uint8_t enterMenu;	/* logical flag */
   uint8_t configRegister;
 
   // -------- Inits --------- //
@@ -55,93 +56,111 @@ int main(void){
   /* Load up last values from EEPROM */
   secondsDelay = EEPROM_readWord(SECONDS_POINTER);
 
-  // ------ Event loop ------ //
-  while(1){     
-
-    // On reset,  enter setup menu
-    if (enterMenu){
-      printString("\r\n====[  Logging Thermometer ]====\r\n  ");
-      currentMemoryLocation = EEPROM_readWord(CURRENT_LOCATION_POINTER);
-      print16bits(currentMemoryLocation - MEMORY_START);
-      printString(" readings in log.\r\n  ");
-      print16bits(secondsDelay);
-      printString(" seconds between readings.\r\n");
-      printString(" [<] to shorten sample delay time\r\n");
-      printString(" [>] to increase sample delay time\r\n");
-      printString(" [?] to reset delay time to 60 sec\r\n");
-      printString(" [d] to print out log over serial\r\n");
-      printString(" [e] to erase memory\r\n");
-      printString(" [s] to start logging\r\n\r\n");
-   
-      switch(receiveByte()){
-      case 'd':
-	SLAVE_SELECT;
-	SPI_tradeByte(EEPROM_READ);
-	EEPROM_send16BitAddress(MEMORY_START);
-	for(i=MEMORY_START; i<currentMemoryLocation; i++){
-	  SPI_tradeByte(0);
-	  printTemperature(SPDR);
-	}
-	SLAVE_DESELECT;
-	break;
-      case '<':
-	if (secondsDelay >= 10){
-	  secondsDelay-=5;
-	  EEPROM_writeWord(SECONDS_POINTER, secondsDelay);
-	}
-	break;
-      case '>':
-	if (secondsDelay < 65000){
-	  secondsDelay+=5;
-	  EEPROM_writeWord(SECONDS_POINTER, secondsDelay);
-	}
-	break;
-      case '?':
-	secondsDelay=60;
+  /* Delay to allow input to enter main menu */
+  printString("*** Press [m] within ");
+  printByte(MENU_DELAY);
+  printString(" seconds to enter menu. ***\r\n ");
+  
+  for (i=0; i<MENU_DELAY; i++){
+    _delay_ms(1000);
+  }
+  
+  if (bit_is_set(UCSR0A, RXC0) && (UDR0 == 'm')){ 
+    enterMenu = 1;
+  }
+  else{
+    enterMenu = 0;
+  }
+  
+  while (enterMenu){
+    printString("\r\n====[  Logging Thermometer ]====\r\n  ");
+    currentMemoryLocation = EEPROM_readWord(CURRENT_LOCATION_POINTER);
+    print16bits(currentMemoryLocation - MEMORY_START);
+    printString(" readings in log.\r\n  ");
+    print16bits(secondsDelay);
+    printString(" seconds between readings.\r\n");
+    printString(" [<] to shorten sample delay time\r\n");
+    printString(" [>] to increase sample delay time\r\n");
+    printString(" [?] to reset delay time to 60 sec\r\n");
+    printString(" [d] to print out log over serial\r\n");
+    printString(" [e] to erase memory\r\n");
+    printString(" [s] to start logging\r\n\r\n");
+    
+    switch(receiveByte()){
+    case 'd':
+      SLAVE_SELECT;
+      SPI_tradeByte(EEPROM_READ);
+      EEPROM_send16BitAddress(MEMORY_START);
+      for(i=MEMORY_START; i<currentMemoryLocation; i++){
+	SPI_tradeByte(0);
+	printTemperature(SPDR);
+      }
+      SLAVE_DESELECT;
+      break;
+    case '<':
+      if (secondsDelay >= 10){
+	secondsDelay-=5;
 	EEPROM_writeWord(SECONDS_POINTER, secondsDelay);
-	break;
-      case 'e':
-	printString("Clearing EEPROM, this could take a few seconds.\r\n");
-	EEPROM_clearAll();
-	EEPROM_writeWord(CURRENT_LOCATION_POINTER, MEMORY_START);	
-	EEPROM_writeWord(SECONDS_POINTER, secondsDelay);	
-	break;
-      case 's':
-	printString("OK, logging...\r\n");
-	enterMenu = 0;
-	break;
-      default:
-	printString("Sorry, didn't understand that.\r\n");
       }
-      
+      break;
+    case '>':
+      if (secondsDelay < 65000){
+	secondsDelay+=5;
+	EEPROM_writeWord(SECONDS_POINTER, secondsDelay);
+      }
+      break;
+    case '?':
+      secondsDelay=60;
+      EEPROM_writeWord(SECONDS_POINTER, secondsDelay);
+      break;
+    case 'e':
+      printString("Clearing EEPROM, this could take a few seconds.\r\n");
+      EEPROM_clearAll();
+      EEPROM_writeWord(CURRENT_LOCATION_POINTER, MEMORY_START);	
+      EEPROM_writeWord(SECONDS_POINTER, secondsDelay);	
+      break;
+    case 's':
+      printString("OK, logging...\r\n");
+      enterMenu = 0;
+      break;
+    default:
+      printString("Sorry, didn't understand that.\r\n");
     }
-    else{		      /* out of menu, get temp and write it */
-      currentMemoryLocation = EEPROM_readWord(CURRENT_LOCATION_POINTER);
-      
-      /* Get Temp from thermometer */
-      i2cStart();                /* Setup and send address, with read bit */
-      i2cSend(LM75_ADDRESS_R);	
-      tempHighByte = i2cReadAck(); /* two bytes of temperature */	
-      tempLowByte  = i2cReadNoAck();	
-      i2cStop();
-      temperatureByte = (tempHighByte << 1) | (tempLowByte >> 7);
-      /* temperatureByte now contains 2x the temperature in Celcius */
-      printTemperature(temperatureByte);           /* serial output */
-      
-      /* Save the new temperature value */
-      EEPROM_writeByte(currentMemoryLocation, temperatureByte); 
-      
-      currentMemoryLocation++;	/* move on and record new position */
+  }
+    
+  // ------ Event loop ------ //
+  while(1){
+    
+    currentMemoryLocation = EEPROM_readWord(CURRENT_LOCATION_POINTER);
+    
+    /* Get Temp from thermometer */
+    i2cStart();                /* Setup and send address, with read bit */
+    i2cSend(LM75_ADDRESS_R);	
+    tempHighByte = i2cReadAck(); /* two bytes of temperature */	
+    tempLowByte  = i2cReadNoAck();	
+    i2cStop();
+    temperatureByte = (tempHighByte << 1) | (tempLowByte >> 7);
+    /* temperatureByte now contains 2x the temperature in Celcius */
+    printTemperature(temperatureByte);           /* serial output */
+    
+    /* Save the new temperature value */
+    EEPROM_writeByte(currentMemoryLocation, temperatureByte); 
+    
+    /* move on to next location and record new position
+       if not already at the end of memory */
+    if (currentMemoryLocation < EEPROM_BYTES_MAX){ 
+      currentMemoryLocation++;	
       EEPROM_writeWord(CURRENT_LOCATION_POINTER, currentMemoryLocation);
-      
-      /*  delay */
-      for (i=0; i<secondsDelay; i++){
-	_delay_ms(1000);
-	toggle_bit(LED_PORT, LED0); /* blink to show working */
-      }
     }
+    
+    /*  delay */
+    for (i=0; i<secondsDelay; i++){
+      _delay_ms(1000);
+      toggle_bit(LED_PORT, LED0); /* blink to show working */
+    }
+
   }    /* End event loop */
-  return(0);                  /* This line is never reached  */
+return(0);                  /* This line is never reached  */
 }
 
 
