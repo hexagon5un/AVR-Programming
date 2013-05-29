d2dc3c3d19cdf11387106af27585eb8c9dc0ad2a
Elliot Williams <elliot@pre.postero.us>
2013-05-29 22:35:00 +0200

    finished up tweaks to cornell code -- not popping now.  woot.

--- talkingPROGMEM/cornell/dpcm_2bit/speech_644_GCC.c
+++ talkingPROGMEM/cornell/dpcm_2bit/speech_644_GCC.c
@@ -1,104 +1,109 @@
-//Voice decompressor example
+//  Talking Voltmeter Example
+
 #include <inttypes.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <avr/pgmspace.h>
+#include "pinDefines.h"
 
-//#include "DPCM_rich_8000_hello_world.h"
-//#define TABLE_NAME  DPCM_rich_8000_hello_world
-#include "DPCM_hiChris_8000.h"
-#define TABLE_NAME  DPCM_hiChris_8000
+#include "DPCM_rich_hello_world_8000.h"
+#define TABLE_NAME  DPCM_rich_hello_world_8000
 
-//reconstruction differentials 
-//volatile signed char PCMvalue[4] = {-78, -16, 16, 78};
+volatile uint16_t sampleNumber;         // sample index
+volatile int8_t   out, lastout;		// output values
+volatile uint8_t  p1, p2, p3, p4;	// hold 4 differentials
+const int8_t      PCMvalue[4] = {-20, -4, 4, 20};
 
-volatile signed char PCMvalue[4] = {-20, -4, 4, 20};
-
-volatile unsigned int outI, tableI;     //indexes
-volatile unsigned char cycle ;  		//decode phase counter
-volatile signed char out, lastout;		//output values
-volatile unsigned char p1, p2, p3, p4;	//hold 4 differentials
-volatile unsigned char packed	;		//byte containing 4 2-bit values	
-
-
-//generate waveform at 7812 scamples/sec
 ISR (TIMER2_COMPA_vect){ 
-  PORTB ^= (1<<PB0); 
-  //compute next sample
-  cycle = outI & 3;  // outI modulo 4
-  if (cycle==0)      //do we need to unpack more data?
-    {
-      if (tableI<sizeof(TABLE_NAME))  //end of stored wave?
-        {
-	  //unpack a table entry into 2-bit indexs
-	  // pgm_read_byte (address_short) 
-	  packed = pgm_read_byte(&TABLE_NAME[tableI]) ;
-	  //packed = DPCMAllDigits[tableI];
-	  p1 = (packed>>6) & 3 ;
-	  p2 = (packed>>4) & 3 ;
-	  p3 = (packed>>2) & 3 ;
-	  p4 = (packed & 3); 
-	  tableI++ ; 
-        } //end unpack table entry 
-      //compute the output and send to PWM
-      out = lastout + PCMvalue[p1] - (lastout>>3) ;  	
+  /* Timer 2 controls sampling speed -- 
+     ISR reads new data, loads PWM output, OCR0A */
+  /* Since we can decode 4 2-bit values at once, need to know where
+     we are in the 4-step mini-cycle. */
+  uint8_t cycle = sampleNumber & 0b00000011; 	/* our 4 steps */
+  uint16_t tableEntry = sampleNumber >> 2;	/* 4 steps per table byte */
+  uint8_t  packedData;				/* the new byte */
+  switch(cycle){
+  case 0:  // Start of the cycle, unpack next byte of samples
+    if (tableEntry < sizeof(TABLE_NAME)){  
+      packedData = pgm_read_byte(&TABLE_NAME[tableEntry]) ;
+      p1 = (packedData>>6) & 3 ;
+      p2 = (packedData>>4) & 3 ;
+      p3 = (packedData>>2) & 3 ;
+      p4 = (packedData & 3); 
     }
-  else if (cycle==1)    //don't need to unpack yet--just ouput
+    /* Add in the next PCM differential value */
+    out = lastout + PCMvalue[p1] - (lastout>>3) ;  	
+    break;
+  case 1:
     out = lastout + PCMvalue[p2] - (lastout>>3) ;
-  else if (cycle==2)
+    break;
+  case 2:
     out = lastout + PCMvalue[p3] - (lastout>>3) ; 
-  else if (cycle==3) 
+    break;
+  case 3:
     out = lastout + PCMvalue[p4] - (lastout>>3) ;
-  
-  //update outputs
-  OCR0A = out + 128;
-  lastout = out;
-  outI++;
-  //at end, turn off Timer0
-  if (tableI==sizeof(TABLE_NAME)) {
-    TCCR0B &= ~(1<<CS00);	
-    OCR0A = 128;
+    break;
   }
-} //ISR
+  /* Update PWM audio output */
+  OCR0A = out + 128;		/* re-center for 0-255 PWM */
+  lastout = out;		/* update last value */
+  sampleNumber++;		/* on to next sample */
+  
+  /* When done, turn off PWM, Timer0 */
+  if (sampleNumber == 4*sizeof(TABLE_NAME)-1) {
+    TCCR2B = 0;			/* disable sample-playback clock */
+    OCR0A = 128;	       	/* idle at mid-voltage */
+    lastout = 0;		/* start at 0 next time */
+  }
+} // end ISR
+
+void initTimer0(void){
+  // Timer 0 Configured for free-running PWM Audio Output
+  TCCR0A |= (1<<WGM00) | (1<<WGM01); /* fast PWM mode */
+  TCCR0A |= (1<<COM0A0) | (1<<COM0A1); /* output on PD6/OC0A */
+  TCCR0B = (1<<CS00);		       /* fastest clock */
+  OCR0A = 128 ;			       /* initialize mid-value */
+  SPEAKER_DDR |= (1<<SPEAKER);	       /* output PD6 / OC0A */
+}
+
+void initTimer2(void){
+  // Timer 2 loads OCR0A, defines sampling frequency
+  // Aiming for around 8kHz
+  TCCR2A = (1<<WGM21);	      /* CTC, count to OCR2A */
+  OCR2A = 128;		      /* controls sample playback frequency */
+  TCCR2B = (1<<CS21);	      /* clock source / 8 = 1MHz */
+  TIMSK2 = (1<<OCIE2A);	      /* turn on compare interrupt */
+  sei();
+}
+
+void initADC(void){
+  // ADC for Voltmeter function
+  ADMUX  |= (0b00001111 & PC5);  /* set mux to ADC5 */
+  DIDR0 |= _BV(ADC5D);		 /* turn off digital circuitry on PC5 */
+  ADMUX  |= (1 << REFS0);        /* reference voltage is AVCC, 5V */
+  ADCSRA |= (1 << ADPS1) | (1 << ADPS2); /* ADC clock prescaler /64 */
+  ADCSRA |= (1 << ADEN);        /* enable ADC */
+}
 
 int main(void){ 
   uint8_t adcValue;
-  DDRD |= (1<<PD6); 
-  // turn on pwm with period= 256 cycles 
-  // (62,500 samples/sec) in fast PWM mode.
-  // BUT OCR0A update is done using timer2 at 7800/sec
-  // timer 0 runs at full rate set in MAIN loop; TCCR0B = 1 ;
-  // turn on fast PWM and OC0A output
-  // 16 microsec per PWM cycle sample time
-  TCCR0A = (1<<COM0A0) | (1<<COM0A1) | (1<<WGM00) | (1<<WGM01) ; 
-  TCCR0B = (1<<CS00); 
-  OCR0A = 128 ; // initialize PWM to half full scale
   
-  // turn on timer2 set to overflow at 7812 Hz 
-  // (prescaler set to divide by 8)
-  TCCR2A = (1<<WGM21);		/* CTC, count to OCR2A */
-  OCR2A = 128;			/* control sample playback frequency */
-  TCCR2B = (1<<CS21); 		/* clock source / 8 = 1MHz */
-  TIMSK2 = (1<<OCIE2A);   // turn on compare interrupt
-  
-  sei();
-  
+  initTimer0();
+  initTimer2();
+  initADC();
+
   while(1) {  
-    //init the output indexes
-    outI = 0; 
-    tableI = 0; 
-    //init the ouptut value
-    lastout = 0; 
-    // turn on PWM
-    TCCR0B = (1<<CS00); 
-    //wait until the speech is done then
-    //time delay the next utterance.
-    loop_until_bit_is_clear(TCCR0B, CS00); 
+    sampleNumber = 0;		/* back to start of sample table */
+    TCCR2B = (1<<CS21);		/* start loading samples */
+    
+    /* Wait until done speaking, then delay some more. */
+    loop_until_bit_is_clear(TCCR2B, CS21);
     _delay_ms(1000);
-       
-  } // end while  
-}  //end main
+    
+  } /*  end while  */
+  return(0);
+}  
 
 
 
