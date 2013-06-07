8b7c4f37a43a9d069ed76c6ebb834360f1a1278e
Elliot Williams <elliot@pre.postero.us>
2013-06-07 18:29:55 +0200

    better working version.  nicer menu interaction

--- eepromRepeater/eepromRepeater.c
+++ eepromRepeater/eepromRepeater.c
@@ -20,7 +20,7 @@ volatile uint8_t* selectedPattern;
 uint8_t  notemap[96];
 
 
-uint8_t  EEMEM pat1[] = {C1,C1,C1,C1,0,0,0,0,
+uint8_t  EEMEM pat1[PATTERN_LEN] = {C1,C1,C1,C1,0,0,0,0,
 			 C2,0,0,0,0,0,0,0,
 			 C2,0,0,0,0,0,0,0,
 			 C2,0,0,0,0,0,0,0,
@@ -37,7 +37,7 @@ uint8_t  EEMEM pat1[] = {C1,C1,C1,C1,0,0,0,0,
 			 C2,0,0,0,0,0,0,0,
 			 C2,0,0,0,0,0,0,0
 };
-uint8_t EEMEM  pat2[] = {D1,D1,D1,D1,0,0,0,0,
+uint8_t EEMEM  pat2[PATTERN_LEN] = {D1,D1,D1,D1,0,0,0,0,
 			 D2,0,0,0,0,0,0,0,
 			 D2,0,0,0,0,0,0,0,
 			 D2,0,0,0,0,0,0,0,
@@ -54,7 +54,7 @@ uint8_t EEMEM  pat2[] = {D1,D1,D1,D1,0,0,0,0,
 			 D2,0,0,0,0,0,0,0,
 			 D2,0,0,0,0,0,0,0
 };
-uint8_t EEMEM pat3[] = {E1,E1,E1,E1,0,0,0,0,
+uint8_t EEMEM pat3[PATTERN_LEN] = {E1,E1,E1,E1,0,0,0,0,
 			E2,0,0,0,0,0,0,0,
 			E2,0,0,0,0,0,0,0,
 			E2,0,0,0,0,0,0,0,
@@ -71,7 +71,7 @@ uint8_t EEMEM pat3[] = {E1,E1,E1,E1,0,0,0,0,
 			E2,0,0,0,0,0,0,0,
 			E2,0,0,0,0,0,0,0
 };
-uint8_t  EEMEM pat4[]= {G1,G1,G1,G1,0,0,0,0,
+uint8_t  EEMEM pat4[PATTERN_LEN]= {G1,G1,G1,G1,0,0,0,0,
 			G2,0,0,0,0,0,0,0,
 			G2,0,0,0,0,0,0,0,
 			G2,0,0,0,0,0,0,0,
@@ -121,8 +121,33 @@ void playPattern(uint8_t* whichPattern){
   patternStep = 0;
   set_bit(TIMSK2, OCIE2A);	      /* enable T2 interrupt */
   while(patternStep < PATTERN_LEN){
-    toggle_bit(LED_PORT, LED0);
+    if ((patternStep & 0b00000111) == 1){ 
+      toggle_bit(LED_PORT, LED0);
+    }
     currentNote = eeprom_read_byte(whichPattern + patternStep);
+    if (currentNote){		/* if there is a note, play it */
+      OCR0A = currentNote;
+      set_bit(SPEAKER_DDR, SPEAKER);
+    }
+    else{			/* otherwise, turn output off */
+      clear_bit(SPEAKER_DDR, SPEAKER);
+    }
+    thisStep = patternStep;
+    while(thisStep == patternStep){;} /* wait until next step */
+  } 
+  clear_bit(LED_PORT, LED0);
+}
+
+void playBuffer(uint8_t buffer[]){
+  uint8_t currentNote;
+  uint8_t thisStep;
+  patternStep = 0;
+  set_bit(TIMSK2, OCIE2A);	      /* enable T2 interrupt */
+  while(patternStep < PATTERN_LEN){
+    if ((patternStep & 0b00000111) == 1){ 
+      toggle_bit(LED_PORT, LED0);
+    }
+    currentNote = buffer[patternStep];
     if (currentNote){		/* if there is a note, play it */
       OCR0A = currentNote;
       set_bit(SPEAKER_DDR, SPEAKER);
@@ -142,7 +167,9 @@ void recordPattern(uint8_t* buffer){
   patternStep = 0;
   set_bit(TIMSK2, OCIE2A);	      /* enable T2 interrupt */
   while(patternStep < PATTERN_LEN){	/* loop until done */
-    toggle_bit(LED_PORT, LED0);
+    if ((patternStep & 0b00000111) == 1){ 
+      toggle_bit(LED_PORT, LED0);
+    }
     if (bit_is_set(UCSR0A, RXC0)){	/* just got bit */
       currentNote = notemap[UDR0-' '];
       buffer[patternStep] = currentNote;
@@ -164,7 +191,7 @@ void clickTrack(void){
   set_bit(LED_PORT, LED0);
   patternStep = 0;
   set_bit(TIMSK2, OCIE2A);	      /* enable T2 interrupt */
-  while(patternStep < PATTERN_LEN){	/* loop until done */
+  while(patternStep < PATTERN_LEN/2){	/* loop until done */
     toggle_bit(LED_PORT, LED0);
     if ((patternStep & 0b00001111) == 1){ /* only click every 16th sample */
       OCR0A = 50;
@@ -227,6 +254,7 @@ int main(void){
   uint8_t recordBuffer[PATTERN_LEN];
   char input1;
   char input2;
+  uint8_t* patternArray[4] = {pat1, pat2, pat3, pat4};
   initNotemap();
   initTimer0();
   initTimer2();
@@ -237,8 +265,8 @@ int main(void){
   while(1){
     
     printString("\r\n------  Playback / Recorder  ------\r\n");
-    printString(" [r] + 1-4 to record a track\r\n");
-    printString(" [p] + 1-4 to play a track once\r\n");
+    printString(" [r] to record a track\r\n");
+    printString(" [1-4] to play back a track\r\n");
     printString(" [q] to toggle free play\r\n");
     input1 = receiveByte();
     switch(input1){
@@ -246,47 +274,32 @@ int main(void){
       freePlay();
       break;
     case 'r':
-      printString("Select a track [1-4]: ");
-      input2 = receiveByte();
       clickTrack();
       recordPattern(recordBuffer);
-      switch(input2){
-      case '1':
-	eeprom_update_block(recordBuffer, pat1, PATTERN_LEN);
-	break;
-      case '2':
-	eeprom_update_block(recordBuffer, pat2, PATTERN_LEN);
-	break;
-      case '3':
-	eeprom_update_block(recordBuffer, pat3, PATTERN_LEN);
-	break;
-      case '4':
-	eeprom_update_block(recordBuffer, pat4, PATTERN_LEN);
-	break;
-      default:
-	break;
-      }
+      do{
+	printString("\r\n[r]eplay, save to EEPROM [1-4], or discard? ");
+	input2 = receiveByte();
+	if (input2 >= '1' && input2 <= '4'){
+	  eeprom_update_block(recordBuffer, patternArray[input2-'1'], PATTERN_LEN);
+	}
+	if (input2 == 'r'){
+	  playBuffer(recordBuffer);
+	}
+      } while(input2 == 'r');
       break;
-    case 'p':
-      printString("Select a track [1-4]: ");
-      input2 = receiveByte();
-      switch(input2){
-      case '1':
-	playPattern(pat1);
-	break;
-      case '2':
-	playPattern(pat2);
-	break;
-      case '3':
-	playPattern(pat3);
-	break;
-      case '4':
-	playPattern(pat4);
-	break;
-      default:
-	break;
-      }
+    case '1':
+      playPattern(patternArray[0]);
       break;
+    case '2':
+      playPattern(patternArray[1]);
+      break;
+    case '3':
+      playPattern(patternArray[2]);
+      break;
+    case '4':
+      playPattern(patternArray[3]);
+      break;
+      
     default:
       break;
     }
