356aeed4bdb9b76e204a95fb2afbf4e4ea733cdb
Elliot Williams <elliot@pre.postero.us>
2013-06-07 11:10:44 +0200

    fixing up makefiles to comment out printf libraries

--- eepromRepeater/eepromRepeater.c
+++ eepromRepeater/eepromRepeater.c
@@ -9,37 +9,41 @@
 #include "pinDefines.h"
 #include "macros.h"
 #include "UART.h"
-
-// -------- Global Variables --------- //    
-char  EEMEM welcomeString[]= "Welcome to the EEPROM Repeater.";
-uint8_t EEMEM testVariable = 9;
+#include "scale8.h"             /* 8-bit scale */
 
 
 // -------- Functions --------- //
+static inline void initTimer(void){
+  set_bit(TCCR0A, WGM01);       /* CTC mode */
+  set_bit(TCCR0A, COM0A0);      /* Toggles pin each cycle through */
+  set_bit(TCCR0B, CS00);
+  set_bit(TCCR0B, CS01);        /* CPU clock / 64 */
+}
+
+static inline void playNote(uint8_t wavelength, uint16_t duration){
+  OCR0A = wavelength;           /* set pitch */
+  set_bit(SPEAKER_DDR, SPEAKER);/* enable output on speaker */
+
+  while(duration){              /* Variable delay */
+    _delay_ms(1);
+    duration--;
+  }
+  clear_bit(SPEAKER_DDR, SPEAKER); /* turn speaker off */
+}
 
 int main(void){
 
   // -------- Inits --------- //
  
+  initTimer();
   initUART();
-  
-  
-  /* // Store the 8-bit value 5 in EEPROM slot 0 */
-  /* uint8_t* address = 0; */
-  /* eeprom_update_byte(address, 5); */
-  
-  /* // Store the 16-bit value 12345 in EEPROM slots 1 and 2: */
-  /* eeprom_update_word((uint16_t*) 1, 12345); */
-  
-  /* // Store a character array (string) in EEPROM slot 3-15: */
-  /* char myString[] = "hello world.";  */
-  /* eeprom_update_block(&myString, (void*) 3, sizeof(myString)); */
+  printString("hello");
+  while(1){
+    
+    playNote(C2,50);
+    _delay_ms(1000);
 
-  
-
-  printByte(eeprom_read_byte(&testVariable));
-  printByte(eeprom_read_byte(&welcomeString));
-   
+  }   
   // ------ Event loop ------ //
   return(0);                  /* This line is never reached  */
 }
