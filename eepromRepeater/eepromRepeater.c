6308516e065b2e923ce0bb76a9664e34593ab41b
Elliot Williams <elliot@pre.postero.us>
2013-06-07 13:19:22 +0200

    first pattern demo

--- eepromRepeater/eepromRepeater.c
+++ eepromRepeater/eepromRepeater.c
@@ -12,37 +12,92 @@
 #include "scale8.h"             /* 8-bit scale */
 
 
+// Define EEMEM variables
+#define PATTERN_LEN   128	/* 16 * 8 */
+uint8_t pattern1[PATTERN_LEN] EEMEM = {C1,C1,C1,C1,0,0,0,0,
+				       C2,0,0,0,0,0,0,0,
+				       C2,0,0,0,0,0,0,0,
+				       C2,0,0,0,0,0,0,0,
+				       C1,C1,C1,C1,0,0,0,0,
+				       C2,0,0,0,0,0,0,0,
+				       C2,0,0,0,0,0,0,0,
+				       C2,0,0,0,0,0,0,0,
+				       C1,C1,C1,C1,0,0,0,0,
+				       C2,0,0,0,0,0,0,0,
+				       C2,0,0,0,0,0,0,0,
+				       C2,0,0,0,0,0,0,0,
+				       C1,C1,C1,C1,0,0,0,0,
+				       C2,0,0,0,0,0,0,0,
+				       C2,0,0,0,0,0,0,0,
+				       C2,0,0,0,0,0,0,0
+				       };
+uint8_t pattern2[PATTERN_LEN] EEMEM;
+uint8_t pattern3[PATTERN_LEN] EEMEM;
+uint8_t pattern4[PATTERN_LEN] EEMEM;
+volatile uint8_t patternStep;
+volatile uint8_t* selectedPattern;
+
 // -------- Functions --------- //
-static inline void initTimer(void){
-  set_bit(TCCR0A, WGM01);       /* CTC mode */
-  set_bit(TCCR0A, COM0A0);      /* Toggles pin each cycle through */
-  set_bit(TCCR0B, CS00);
-  set_bit(TCCR0B, CS01);        /* CPU clock / 64 */
+// Audio output on timer0
+static inline void initTimer0(void){
+  TCCR0A |= (1<<WGM01) | (1<<COM0A0); /* CTC, output on pin */
+  TCCR0B |= (1<<CS00) | (1<<CS01);    /* set prescaler */
 }
 
-static inline void playNote(uint8_t wavelength, uint16_t duration){
-  OCR0A = wavelength;           /* set pitch */
-  set_bit(SPEAKER_DDR, SPEAKER);/* enable output on speaker */
+//  timer2
+static inline void initTimer2(void){
+  TCCR2A |= (1<<WGM21) ;	   /* CTC */
+  TCCR2B |= (1<<CS22)|(1<<CS21)|(1<<CS20); /* set prescaler /1024 */
+  OCR2A = 255;				   /* sets note speed */
+  TIMSK2 |= (1<<OCIE2A);	/* set compare interrupt */
+  sei();
+}
 
-  while(duration){              /* Variable delay */
-    _delay_ms(1);
-    duration--;
+ISR(TIMER2_COMPA_vect){
+  uint8_t currentNote = 0;
+  if (patternStep < PATTERN_LEN){
+    currentNote = eeprom_read_byte(&selectedPattern[patternStep]);
+    if (currentNote){
+      OCR0A = currentNote;
+      set_bit(SPEAKER_DDR, SPEAKER);
+    }
+    else{
+      clear_bit(SPEAKER_DDR, SPEAKER);
+    }
+    patternStep++;
   }
-  clear_bit(SPEAKER_DDR, SPEAKER); /* turn speaker off */
+  else{
+    patternStep = 0;
+  }
+  
 }
 
 int main(void){
 
   // -------- Inits --------- //
  
-  initTimer();
+  initTimer0();
+  initTimer2();
   initUART();
-  printString("hello");
-  while(1){
-    
-    playNote(C2,50);
-    _delay_ms(1000);
+  printWord(&pattern1);  
+  printString("\r\n");
+  printWord(&pattern2);  
+  printString("\r\n");
+  printWord(&pattern3);  
+  printString("\r\n");
+  printWord(&pattern4);  
+  printString("\r\n");
 
+  while(1){
+    selectedPattern = pattern1;
+
+    _delay_ms(1000);
+    _delay_ms(1000);
+    _delay_ms(1000);
+    //selectedPattern = pattern2;
+    //selectedPattern = pattern3;
+    //selectedPattern = pattern4;
+   
   }   
   // ------ Event loop ------ //
   return(0);                  /* This line is never reached  */
