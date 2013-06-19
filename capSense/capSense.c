dc585a67de2ff3c5f308ee1006fa143489a51037
Elliot Williams <elliot@pre.postero.us>
2013-06-19 21:39:08 +0200

    tweaks to capSense.  awesome now.

--- capSense/capSense.c
+++ capSense/capSense.c
@@ -28,13 +28,13 @@ volatile uint16_t chargeCycleCount;   /* internal ISR use only */
 ISR(PCINT1_vect){
   chargeCycleCount++;		        /* count this change */
   
-  set_bit(CAP_SENSOR_PORT, CAP_SENSOR); /* set high to charge capacitor */
-  set_bit(CAP_SENSOR_DDR, CAP_SENSOR);  /* output mode */
+  CAP_SENSOR_PORT |= (1 << CAP_SENSOR); /* set high to charge capacitor */
+  CAP_SENSOR_DDR |= (1 << CAP_SENSOR);  /* output mode */
   _delay_us(1);			        /* charging delay */
     
-  clear_bit(CAP_SENSOR_DDR, CAP_SENSOR);   /* set as input */
-  clear_bit(CAP_SENSOR_PORT, CAP_SENSOR);  /* with no pullup */
-  set_bit(PCIFR, PCIF1);		   /* clear the pin-change interrupt */
+  CAP_SENSOR_DDR &= ~(1 << CAP_SENSOR);   /* set as input */
+  CAP_SENSOR_PORT &= ~(1 << CAP_SENSOR);  /* with no pullup */
+  PCIFR |= (1 << PCIF1);		   /* clear the pin-change interrupt */
 }
 
 /* This interrupt runs on regular schedule, decides if pressed */
@@ -56,25 +56,24 @@ ISR(TIMER0_COMPA_vect){
   }  
 
   /* Reset */
-  chargeCycleCount = 0;
+  chargeCycleCount = 0;  
   /* Charge capacitor up at least once per period, 
      just to make sure still cycling */
-  set_bit(CAP_SENSOR_DDR, CAP_SENSOR);
-  set_bit(CAP_SENSOR_PORT, CAP_SENSOR);
+  CAP_SENSOR_DDR |= (1 << CAP_SENSOR);
+  CAP_SENSOR_PORT |= (1 << CAP_SENSOR);
 }
 
-static inline void initTimer0(void){
-  set_bit(TCCR0A, WGM01);       /* CTC mode */
-  set_bit(TCCR0B, CS02);        
-  set_bit(TCCR0B, CS00);        /* 8 MHz / 1024 clock */
-  set_bit(TIMSK0, OCIE0A);      /* output compare interrupt enable*/
-  OCR0A = 200;                  /* roughly 25 ms */ 
+void initTimer0(void){
+  TCCR0A |= (1 << WGM01);	     /* CTC mode */
+  TCCR0B |= (1 << CS02)|(1 << CS00); /* 8 MHz / 1024 clock */ 
+  OCR0A = 200;			     /* roughly 25 ms */ 
+  TIMSK0 |= (1 << OCIE0A);      /* output compare interrupt enable*/
   sei();                        /* set enable interrupt bit */
 }
 
-static inline void initPinChangeInterrupt(void){
-  set_bit(PCICR, PCIE1);   /* enable Pin-change interrupts 1 (bank C) */
-  set_bit(PCMSK1, PCINT9); /* enable specific interrupt for our pin PC1 */
+void initPinChangeInterrupt(void){
+  PCICR |= (1 << PCIE1);   /* enable Pin-change interrupts 1 (bank C) */
+  PCMSK1 |= (1 << PCINT9); /* enable specific interrupt for our pin PC1 */
   sei();		       /* set (global) interrupt enable bit */
 }
 
