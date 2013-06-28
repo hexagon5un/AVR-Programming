4a2c8fae55de7974a16c130cdacb3cb4ee4d26b7
Elliot Williams <elliot@pre.postero.us>
2013-06-28 15:04:31 +0200

    rework on accelerometer, sensitivity pot added

--- accelerometer/accelerometer.c
+++ accelerometer/accelerometer.c
@@ -6,17 +6,21 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 #include "pinDefines.h"
-#include "macros.h"
 #include "USART.h"
 
 #define CALIBRATION_SAMPLES  20	 /* number samples in calibration phase */
-#define ON_TICKS             250 /* 2.5 sec, in 1/100 second */
+#define ON_TICKS             500 /* 5 sec, in 1/100 second */
+
+#define SENSITIVITY_POT      0	/* set to 1 if you have pot attached */
+#define SENSITIVITY_ADC      PC5
+
+#define SENSITIVITY_FACTOR   2	/* else let it automatically calibrate */
 
 // -------- Global Variables --------- //    
 volatile uint16_t ticks;	/* for system tick clock */
 
 // -------- Functions --------- //
-static inline void initTicks(){
+void initTicks(){
   TCCR0A |= (1 << WGM01);	  /* CTC mode */
   TCCR0B |= (1<<CS00) | (1<<CS02);/* 8 MHz / 1024 */
   TIMSK0 |= (1<<OCIE0A); 	  /* output compare interrupt enable*/
@@ -29,25 +33,31 @@ ISR(TIMER0_COMPA_vect){
   ticks++;
 }
 
-static inline void initADC(void){
-  ADMUX  |= (0b00001111 & PIEZO);  /* set mux to ADC2 */
+void initADC(void){
   ADMUX  |= (1 << REFS0);        /* reference voltage to AVCC */
   ADCSRA |= (1 << ADPS1) | (1 << ADPS2); /* ADC clock prescaler /64 */
   ADCSRA |= (1 << ADEN);        /* enable ADC */
 }
 
-static inline void sleepDelay(uint16_t numTicks){
-  numTicks += ticks;  	   /* when should I wake back up? */
-  while(ticks < numTicks){
+uint16_t readADC(uint8_t channel){
+  ADMUX = (0b11110000 & ADMUX) | channel;
+  ADCSRA |= (1 << ADSC);
+  loop_until_bit_is_clear(ADCSRA, ADSC);
+  return(ADC);
+}
+
+void sleepDelay(uint16_t numTicks){
+  numTicks += ticks;  	  /* when should I wake back up? */
+  while(ticks < numTicks){  
     sleep_mode();
   }
 }
 
-static inline void blink(uint8_t times){
+void blink(uint8_t times){
   /* Simple loop toggles LEDs on # times */
   while(times){
-    toggle_bit(LED_PORT, LED0);
-    toggle_bit(LED_PORT, LED1);
+    LED_PORT ^= (1<<LED0);
+    LED_PORT ^= (1<<LED1);
     sleepDelay(20);
     times--;
   }
@@ -79,23 +89,22 @@ int main(void){
   /* Sensitivity calibration routine */
   maxValue = 0;			/* start with small max, big min */
   minValue = 1023;
-  set_bit(LED_PORT, LED0);	/* set one LED on initially */
+  LED_PORT |= (1 << LED0);	/* set one LED on initially */
   for (i = 0; i < CALIBRATION_SAMPLES; i++){
-    /* blink alternately while calibrating */
-    blink(1);			
-    /* sample */
-    set_bit(ADCSRA, ADSC);		
-    loop_until_bit_is_clear(ADCSRA, ADSC);
-    adcValue = ADC;
-    /* update max/min */
-    if (adcValue > maxValue){
+    blink(1);	      /* blink alternately while calibrating */	
+    
+    adcValue = readADC(PIEZO); 	/* sample once */
+
+    if (adcValue > maxValue){   /* update max/min */
       maxValue = adcValue;
     }
     else if (adcValue < minValue){
       minValue = adcValue;
     }
   }
-  padding = maxValue - minValue;
+
+  /* noisier background = more padding */
+  padding = SENSITIVITY_FACTOR*(maxValue - minValue); 
   transmitByte((minValue-127)); /* lower eight bits, centered */
   transmitByte((maxValue-127)); 
   /* Blink both to signal done */
@@ -106,30 +115,33 @@ int main(void){
   while(1){     
 
     /* Do conversion */
-    set_bit(ADCSRA, ADSC);
-    loop_until_bit_is_clear(ADCSRA, ADSC);
-    adcValue = ADC;
+    adcValue = readADC(PIEZO);
     transmitByte((adcValue-127)); /* quasi-seismograph output */
+
+#if SENSITIVITY_POT
+    padding = readADC(SENSITIVITY_ADC);	/* read sensitivity from pot */
+    padding = (padding >> 4);	/* optionally scale to useful range */
+#endif    
 
     /* Light up if outside threshold */
     if (adcValue < (minValue - padding)){
       LED_PORT = (1 << LED0) | (1 << LED7); /* one LED, switch */
-      lightsOutTime = ticks + ON_TICKS;
+      lightsOutTime = ticks + ON_TICKS;	    /* leave light on until... */
     }
     else if (adcValue > (maxValue + padding)){
       LED_PORT = (1 << LED1) | (1 << LED7); /* other LED, switch */
-      lightsOutTime = ticks + ON_TICKS;
+      lightsOutTime = ticks + ON_TICKS;     /* leave light on until... */
     }
-    else{ /* Nothing seen, turn off light when it's time */
-      clear_bit(LED_PORT, LED0);
-      clear_bit(LED_PORT, LED1);   /* Both off */
+    else{ /* Nothing seen, turn off lights */
+      LED_PORT &= ~(1 << LED0);
+      LED_PORT &= ~(1 << LED1);   /* Both off */
       if (ticks == lightsOutTime){
-	clear_bit(LED_PORT, LED7); /* turn switch off */
+	LED_PORT &= ~(1 << LED7); /* turn switch off */
 	sleepDelay(10);		   /* delay in case of switch transients */
       }
     }
-
-    sleep_mode();		/* sleep for the rest of the system tick */
+    
+    //sleep_mode();		/* sleep for the rest of the system tick */
 
   }    /* End event loop */
   return(0);                  /* This line is never reached  */
