0ae8222dd6bede0a58f404de12cf0f09a86e3fe7
Elliot Williams <elliot@pre.postero.us>
2013-07-16 22:58:03 +0200

    couple more comments on sundial

--- servoSundial/calibrateTime.py
+++ servoSundial/calibrateTime.py
@@ -7,18 +7,19 @@ def readTime(serialPort):
     '''Reads the time from the AVR over the serial port'''
     serialPort.flushInput()
     character = ""
-    while(not character == "\n"):                # loop until see end of line character
+    while(not character == "\n"):     # loop until see end of line 
         character = serialPort.read(1)
+    ## The time string looks something like '011:045:023\r\n'
     timeString = serialPort.read(13) 
     hms = timeString.split(":")
-    hms = [int(x) for x in hms]
+    hms = [int(x) for x in hms] # make hour, minute, second numeric
     return(hms)
 
 def setTime(serialPort, hours, minutes, seconds):
     '''Sends the time over the serial port'''
     serialPort.flushOutput()
     serialPort.write("S")
-    time.sleep(0.2)             # delay while AVR sends
+    time.sleep(0.1)             # delay while AVR sends
     serialPort.write(str(hours) + "\r")
     time.sleep(0.2)             # delay while AVR sends
     serialPort.write(str(minutes) + "\r")
@@ -27,14 +28,14 @@ def setTime(serialPort, hours, minutes, seconds):
     
 def setTimeNow(serialPort):
     '''Sets the AVR clock to the current time'''
-    hours, minutes, seconds = time.localtime()[3:6]
+    hours, minutes, seconds = time.localtime()[3:6]    
     setTime(serialPort, hours, minutes, seconds)
     return(time.time())
 
 def calculateTimeDelay(serialPort):
     '''Gets AVR time and subtracts off actual (computer) time'''
     avrHMS = readTime(serialPort)
-    hms =    time.localtime()[3:6]
+    hms = time.localtime()[3:6]
     hmsDifference = [x - y for x,y in zip(avrHMS, hms)]
     out = "AVR is fast by: {x[0]} hours, {x[1]} minutes, and {x[2]} seconds"
     print out.format(x=hmsDifference)
@@ -43,10 +44,10 @@ def calculateTimeDelay(serialPort):
 def calculateTimeDrift(serialPort, startTime):
     '''Calculates the ratio to multiply OVERFLOWS_PER_SECOND 
     given a start time and current error'''
-    elapsed = time.time() - startTime
-    print "After {:.0f} seconds, ".format(elapsed)
     h, m, s = calculateTimeDelay(serialPort)
     driftSeconds = 60*60*h + 60*m + s
+    elapsed = time.time() - startTime
+    print "After {:.0f} seconds, ".format(elapsed)
     return (driftSeconds / elapsed + 1)
  
 
@@ -61,20 +62,25 @@ if __name__ == "__main__":
 
     s = serial.Serial("/dev/ttyUSB0", 9600, timeout=5)
     print "Setting time to current time...."
-    startTime = setTimeNow(s)
-        
+    ratio = 0
+    while not ratio == 1:       # make sure starting time is right on
+        startTime = setTimeNow(s) 
+        ratio = calculateTimeDrift(s, startTime)
+
     ## Note: you can either leave this running or 
     ## you can re-run calculateTimeDrift() at any time in the future, 
     ## as long as you don't overwrite the original startTime 
     while(True):
-        time.sleep(SLEEP_TIME)
         ratio = calculateTimeDrift(s, startTime)
         ratioLog.append([time.time()-startTime, ratio])
         newOverflow = int(OVERFLOWS_PER_SECOND * ratio)
-        print "OVERFLOWS_PER_SECOND should be {}".format(newOverflow)
+        print "OVERFLOWS_PER_SECOND should be {}\n\n".format(newOverflow)
+        time.sleep(SLEEP_TIME)
         
     ## As you leave this routine running, you should see it bounce
     ##  around a lot in the beginning and then settle down after
     ##  running a few hours.  Ironically, it'll converge to a good
     ##  number faster if it's initially very out of sync.  (If it
     ##  drifts faster, you can figure out the drift rate sooner.)
+    ## Leave it running for 24 hours and you'll get one-second-per-day 
+    ##  accuracy.
--- servoSundial/servoSundial.c
+++ servoSundial/servoSundial.c
@@ -10,14 +10,11 @@
 // ------- Defines -------- //
 #define PULSE_MIN         1000  /* experiment with these values */
 #define PULSE_MAX         2000  /* to match your own servo */
-#define PULSE_MID         1500
 #define PULSE_RANGE       (PULSE_MAX - PULSE_MIN)
 
 #define START_TIME        10	/* 10 am */
 #define STOP_TIME 	  22	/* 10 pm */
-#define TIME_RANGE        (STOP_TIME - START_TIME - 1)
-
-#define SERVO_PULSE_INCREMENT  (PULSE_RANGE / (TIME_RANGE * 60)) 
+#define HOURS_RANGE        (STOP_TIME - START_TIME - 1)
 
 #define LASER             PB2
 #define LASER_PORT        PORTB
@@ -32,38 +29,43 @@
 // -------- Global Variables --------- //    
 volatile uint16_t ticks;
 volatile uint8_t  hours   = 15;		/* arbitrary default time */
-volatile uint8_t  minutes = 59;		
-volatile uint8_t  seconds = 52;		
+volatile uint8_t  minutes = 42;		
+volatile uint8_t  seconds = 57;		
 
 // -------- Functions --------- //
 void printTime(uint8_t hours, uint8_t minutes, uint8_t seconds);
 void pollSerial(void);
 
+// Servo setup and utility functions
 void initTimer1_Servo(void){
   /* Set up Timer1 (16bit) to give a pulse every 20ms*/
   TCCR1A |= (1 << WGM11); /* Using Fast PWM mode */
   TCCR1B |= (1 << WGM12); /* counter max in ICR1 */
   TCCR1B |= (1 << WGM13); 
   TCCR1B |= (1 << CS11);      /* /8 prescaling -- microsecond steps*/
-  ICR1 = 20000;               /* TOP value = 20ms */
   TCCR1A |= (1 << COM1A1);    /* set output on PB1 / OC1A for servo */
-  OCR1A = PULSE_MID;	      /* default value */
-  SERVO_DDR |= (1 << SERVO);  /* set pin direction to output */
+  ICR1 = 20000;               /* TOP value = 20ms */
 }
 
 void enableServo(void){
   while(TCNT1 < 3000){;}	/* delay until pulse part of cycle done */          
   SERVO_DDR |= (1 << SERVO);	/* enable servo pulses */
 }
+
 void disableServo(void){
   while(TCNT1 < 3000){;}	/* delay until pulse part of cycle done */
    SERVO_DDR &= ~(1 << SERVO);	/* disable servo pulses */
 }
+
 void setServoPosition(void){
-  uint16_t elapsedMinutes = (hours-START_TIME)*60 + minutes;
-  OCR1A = PULSE_MIN + elapsedMinutes * SERVO_PULSE_INCREMENT;
+  uint32_t elapsedMinutes;  	
+  /* using 32 bits b/c elapsedMinutes * PULSE_RANGE will overflow 16 bits */
+  elapsedMinutes = (hours-START_TIME)*60 + minutes;   
+  OCR1A = PULSE_MIN + elapsedMinutes * PULSE_RANGE/(HOURS_RANGE * 60);
+  enableServo();
 }
 
+// Realtime-clock handling functions
 void initTimer0_Clock(void){
   /* Normal mode, just used for the overflow interrupt */
   TCCR0B |= (1 << CS00);        /* 8 MHz clock = ~31250 overflows per second */
@@ -75,18 +77,10 @@ ISR(TIMER0_OVF_vect){
   ticks++;
 }
 
-// Realtime-clock handling functions
 void everyHour(void){
   hours++;
   if (hours > 23){		/* loop around at end of day */
     hours = 0;
-  }
-  // Handle Laser On/Off times
-  if ((hours >= START_TIME) && (hours < STOP_TIME)){
-    LASER_PORT |= (1 << LASER);
-  }
-  else{
-    LASER_PORT &= ~(1 << LASER);
   }
 }
 void everyMinute(void){
@@ -95,11 +89,15 @@ void everyMinute(void){
     minutes = 0;
     everyHour();
   }
-  // If during business hours, set servo to new minute
+  // If during business hours, set servo to new minute 
   // Otherwise, don't need to move motor when laser is off
   if ((hours >= START_TIME) && (hours < STOP_TIME)){
     setServoPosition();
     enableServo();
+    LASER_PORT |= (1 << LASER);	/* make sure laser is on */
+  }
+  else{			       /* make sure laser is off */
+    LASER_PORT &= ~(1 << LASER);
   }
 }
 void everySecond(void){
@@ -122,7 +120,7 @@ int main(void){
   initUSART();
   printString("\r\nWelcome to the Servo Sundial.\r\n");
   printString("Type S to set time.\r\n");
-  
+
   initTimer0_Clock();
   initTimer1_Servo();
   sei();                       /* set enable interrupt bit */
@@ -163,11 +161,15 @@ void pollSerial(void){
       printString("Setting time...\r\n");
       printString("Hour: ");
       hours = getNumber();
-      printString("Minutes: ");
+      printString("\r\nMinutes: ");
       minutes = getNumber();
-      printString("Seconds: ");
+      printString("\r\nSeconds: ");
       seconds = getNumber();      
+      printString("\r\n");
       ticks = 0;
+      if ((hours >= START_TIME) && (hours < STOP_TIME)){
+	setServoPosition();
+      }
     }
   }  
 }
