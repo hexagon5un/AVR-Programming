9a7006b44c692c0cdca0d2ea00ef284a99c56b74
Elliot Williams <elliot@pre.postero.us>
2013-08-10 17:08:33 +0200

    redo of comments in fancy timers

--- fancyTimers/fancyTimers.c
+++ fancyTimers/fancyTimers.c
@@ -1,22 +1,22 @@
-/* Demo of the power of system ticks  */
-/* Eight LEDs blink, each at just-slightly different frequencies */
-/* Creates a fancy phasing pattern  */
+/* Demo of the power of system ticks
+ * Eight LEDs blink, each at just-slightly different frequencies
+ * Creates a fancy phasing pattern  */
 
 // ------- Preamble -------- //
-#include <avr/io.h>             
+#include <avr/io.h>
 #include <avr/interrupt.h>
 #include "pinDefines.h"
 
 // -------- Global Variables --------- //
-volatile uint8_t milliseconds = 0;  
+volatile uint8_t milliseconds = 0;
 
 // -------- Functions --------- //
 void initTimerTicks(void){
-	TCCR0A |= (1 << WGM01);          /* CTC mode */
-	TCCR0B |= (1 << CS00) | (1 << CS02);  /* 8 MHz / 1024 */
-	OCR0A = 7;      /* 8 Mhz / 1024 / 8 = 1.024 ms */
-	TIMSK0 |= (1 << OCIE0A);   /* output compare interrupt enable*/
-	sei();      /* set (global) enable interrupt bit */
+  TCCR0A |= (1 << WGM01);                                  /* CTC mode */
+  TCCR0B |= (1 << CS00) | (1 << CS02);                 /* 8 MHz / 1024 */
+  OCR0A = 7;                            /* 8 Mhz / 1024 / 8 = 1.024 ms */
+  TIMSK0 |= (1 << OCIE0A);          /* output compare interrupt enable */
+  sei();                          /* set (global) enable interrupt bit */
 }
 
 ISR(TIMER0_COMPA_vect){
@@ -29,19 +29,19 @@ int main(void){
 
   // -------- Inits --------- //
   initTimerTicks();
-  LED_DDR = 0xff;    /* all output */
+  LED_DDR = 0xff;                                        /* all output */
 
   // ------ Event loop ------ //
-  while(1){  
-    /* 8 LEDs flashing on different schedules */
+  while(1){
+                             /* 8 LEDs flashing on different schedules */
     for (i=0 ; i<8 ; i++){
       if(milliseconds == ledTime[i]){
         LED_PORT ^= (1 << i);
-        ledTime[i] = milliseconds + 100 + i; 
+        ledTime[i] = milliseconds + 100 + i;
       }
-    }  
-  }    /* End event loop */
-  return(0);          /* This line is never reached  */
+    }
+  }                                                  /* End event loop */
+  return(0);                             /* This line is never reached */
 }
 
 
