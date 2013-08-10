46871bdb52aa51a0b64866cf8308ced8b83881ed
Elliot Williams <elliot@pre.postero.us>
2013-08-10 17:19:14 +0200

    retabbing USART_short

--- learningAVR/USART_short.c
+++ learningAVR/USART_short.c
@@ -1,37 +1,39 @@
 
-/* 
+/*
   Quick and dirty functions that make serial communications work.
 */
 
-#include <avr/io.h>		
+#include <avr/io.h>
 #include "USART.h"
 #include <util/setbaud.h>
 
-void initUSART (void) {			 /* requires BAUD */
-  UBRR0H = UBRRH_VALUE;			 /* defined in setbaud.h */
+void initUSART (void) {                               /* requires BAUD */
+  UBRR0H = UBRRH_VALUE;                        /* defined in setbaud.h */
   UBRR0L = UBRRL_VALUE;
 #if USE_2X
   UCSR0A |= (1 << U2X0);
 #else
   UCSR0A &= ~(1 << U2X0);
 #endif
-  UCSR0B = (1 << TXEN0) | (1 << RXEN0); /* Enable USART transmitter/receiver */
-  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); /* 8 data bits, 1 stop bit */ 
+                                  /* Enable USART transmitter/receiver */
+  UCSR0B = (1 << TXEN0) | (1 << RXEN0);
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);   /* 8 data bits, 1 stop bit */
 
 }
 
 void transmitByte (uint8_t data) {
-  loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait for empty transmit buffer */
-  UDR0 = data;			          /* send data */  
+                                     /* Wait for empty transmit buffer */
+  loop_until_bit_is_set(UCSR0A, UDRE0);
+  UDR0 = data;                                            /* send data */
 }
 
 uint8_t receiveByte (void) {
-  loop_until_bit_is_set(UCSR0A, RXC0); /* Wait for incoming data */
-  return UDR0;			         /* return register value */  
+  loop_until_bit_is_set(UCSR0A, RXC0);       /* Wait for incoming data */
+  return UDR0;                                /* return register value */
 }
 
 
-/* Here are a bunch of useful printing commands */
+// Here are a bunch of useful printing commands
 void printString(const char myString[]){
   uint8_t i=0;
   while(myString[i]){
