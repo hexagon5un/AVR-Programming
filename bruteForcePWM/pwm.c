009c412d44fe829fe08484dfe77c1a236aa0519b
Elliot Williams <elliot@pre.postero.us>
2013-08-11 16:49:29 +0200

    compiling and reformatting brute force pwm

--- bruteForcePWM/pwm.c
+++ bruteForcePWM/pwm.c
@@ -1,43 +1,39 @@
-/*  
-
-Quick and dirty PWM Demo
-
-*/
+/*
+ * Quick and dirty PWM Demo
+ */
 
 // ------- Preamble -------- //
-#include <avr/io.h>		/* Defines pins, ports, etc */
-#include <util/delay.h>		/* Functions to waste time */
+#include <avr/io.h>                        /* Defines pins, ports, etc */
+#include <util/delay.h>                     /* Functions to waste time */
 #include "pinDefines.h"
-#include "macros.h"
 
 #define LED_DELAY 20
 
 int main(void){
-  
+
   uint8_t brightness = 0;
   int8_t direction = 1;
   uint8_t i;
 
   // -------- Inits --------- //
 
-  // Init all LEDs
-  LED_DDR = 0xff;  
+  LED_DDR = 0xff;                                     /* Init all LEDs */
 
   // ------ Event loop ------ //
-  while(1){	
-    
+  while(1){
+
     // PWM
     for (i=0; i<255; i++){
       if (i < brightness){
-	LED_PORT = 0xff;	/* turn on */
+        LED_PORT = 0xff;                                    /* turn on */
       }
       else{
-	LED_PORT = 0;		/* turn off */
+        LED_PORT = 0;                                      /* turn off */
       }
       _delay_us(LED_DELAY);
     }
 
-    // Brighten and dim 
+    // Brighten and dim
     if (brightness == 0){
       direction = 1;
     }
@@ -46,9 +42,8 @@ int main(void){
     }
     brightness += direction;
 
-
-  } /* End event loop */
-  return(0);		      /* This line is never reached  */
+  }                                                  /* End event loop */
+  return(0);                             /* This line is never reached */
 }
 
 
