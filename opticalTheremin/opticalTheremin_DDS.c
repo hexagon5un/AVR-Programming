2ddec28ee01cde9e76b85333c76025bab260271a
Elliot Williams <elliot@pre.postero.us>
2013-08-21 21:41:40 +0200

    print versions of the theremin code, split into chunks

---
+++ opticalTheremin/opticalTheremin_DDS.c
@@ -0,0 +1,25 @@
+#include "fullTri7.h"
+
+// -------- Global Variables --------- //
+
+volatile uint16_t accumulator;
+volatile uint16_t tuningWord;
+volatile uint8_t volume;
+
+// -------- Functions --------- //
+
+static inline void initTimer0(void) {
+                                     /* Fast PWM mode, output on OCR0A */
+  TCCR0A |= ((1 << WGM00) | (1 << WGM01) | (1 << COM0A1));
+  TCCR0B |= (1 << CS00);                    /* Clock with /1 prescaler */
+  TIMSK0 |= (1 << TOIE0);                        /* Overflow interrupt */
+}
+
+ISR(TIMER0_OVF_vect) {
+  int8_t mixer;
+  mixer = fullTri7[(uint8_t) (accumulator >> 8)];        /* lookup DDS */
+  mixer = ((mixer * volume) >> 8);   /* multiply by volume and rescale */ 
+  OCR0A = 128 + mixer;                              /* recenter output */
+  accumulator += tuningWord;          /* take tuningWord steps forward */
+}
+
---
+++ opticalTheremin/opticalTheremin_main.c
@@ -0,0 +1,42 @@
+/*
+ *Using two light sensors (pitch and volume),
+ *and our previous PWM DDS audio routines,
+ *plus a whole ton of ISRs,
+ *we can make a fairly decent-sounding Theremin-like instrument.
+ */
+
+// ------- Preamble -------- //
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "pinDefines.h"
+#include "fullTri7.h"
+
+// -------- Functions --------- //
+
+static inline void initTimer0(void);
+ISR(TIMER0_OVF_vect);
+static inline void initADC(void);
+ISR(ADC_vect); 
+
+int main(void) {
+
+  // -------- Inits --------- //
+
+  initTimer0();
+  SPEAKER_DDR |= (1 << SPEAKER);           /* enable output to speaker */
+
+  initADC();
+  sei();                                      /* Enable all interrupts */
+  ADCSRA |= (1 << ADSC);                          /* start conversions */
+
+  // ------ Event loop ------ //
+
+  while (1) {
+    /*
+     * empty event loop
+     * with comment haiku
+     * invites further coding
+     */
+  }
+  return (0);
+}
