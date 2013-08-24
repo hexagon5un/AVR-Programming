9552908aaacafea7ccb8b813ca96fbfef1de4ede
Elliot Williams <elliot@pre.postero.us>
2013-08-24 16:23:32 +0200

    temp save -- going to work on moving-average footstep detector

--- footstepDetector/accelerometer.c
+++ footstepDetector/accelerometer.c
@@ -59,27 +59,6 @@ void calibratePiezo(void) {
   transmitByte((maxValue - 127));
   sleepDelay(200);
 }
-
-void initTicks() {
-  TCCR0A |= (1 << WGM01);                                  /* CTC mode */
-  TCCR0B |= (1 << CS00) | (1 << CS02);                 /* 8 MHz / 1024 */
-  TIMSK0 |= (1 << OCIE0A);          /* output compare interrupt enable */
-  OCR0A = 77;                        /* 78 / (8MHz / 1024) =  9.984 ms */
-  sei();                          /* set (global) enable interrupt bit */
-  ticks = 0;
-}
-
-ISR(TIMER0_COMPA_vect) {
-  ticks++;
-}
-
-void sleepDelay(uint16_t numTicks) {
-  numTicks += ticks;                    /* when should I wake back up? */
-  while (!(ticks == numTicks)) {
-    sleep_mode();
-  }
-}
-
 int main(void) {
 
   // -------- Inits --------- //
@@ -102,8 +81,8 @@ int main(void) {
   // ------ Event loop ------ //
   while (1) {
 		adcValue = readADC(PIEZO);                        /* Do conversion */
-		transmitByte((adcValue - 127));        /* quasi-seismograph output */
-									            /* Light up display if outside threshold */
+    transmitByte((adcValue - 127));        /* quasi-seismograph output */
+                              /* Light up display if outside threshold */
     if (adcValue < (minValue - padding)) {
       LED_PORT = (1 << LED0) | (1 << LED7);         /* one LED, switch */
       lightsOutTime = ticks + ON_TICKS;     /* leave light on until... */
