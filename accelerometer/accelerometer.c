78db4460fd6d714cc574b6023c25dd5bb2df2639
Elliot Williams <elliot@pre.postero.us>
2013-08-25 18:32:33 +0200

    done tweaking

--- accelerometer/accelerometer.c
+++ accelerometer/accelerometer.c
@@ -1,6 +1,6 @@
 /*
- * Maximally-sensitive footstep-detector.  
- */
+*      Sensitive footstep-detector.
+*/
 
 // ------- Preamble -------- //
 #include <avr/io.h>
@@ -9,15 +9,16 @@
 #include "pinDefines.h"
 #include "USART.h"
 
-#define ON_TIME            2000             /* milliseconds */
-#define CYCLE_DELAY           9             /* milliseconds */
+#define ON_TIME            2000                        /* milliseconds */
+#define CYCLE_DELAY           9                        /* milliseconds */
 #define INITIAL_PADDING      10
 
-#define SWITCH              PB7 /* LED or light-switch.  It's all in the hookup. */ 
+#define SWITCH              PB7
 
-#define USE_POT               0 /* define to 1 if using potentiometer */ 
+#define USE_POT               0  /* define to 1 if using potentiometer */
+
 #if USE_POT
-  #define POT               PC5  /* optional sensitivity pot on PC5 */ 
+  #define POT               PC5     /* optional sensitivity pot on PC5 */
 #endif
 
 // -------- Functions --------- //
@@ -36,48 +37,47 @@ uint16_t readADC(uint8_t channel) {
 
 int main(void) {
   // -------- Inits --------- //
-  uint16_t lightsOutTime=0;                      /* timer for the switch */
-	uint16_t adcValue;
-	uint16_t middleValue=511;  /* average value */ 
-	uint8_t padding = INITIAL_PADDING;
-
-	/* 2 LEDs as output, "switch" on SWITCH */
+  uint16_t lightsOutTime = 0;                  /* timer for the switch */
+  uint16_t adcValue;
+  uint16_t middleValue = 511;                         /* average value */
+  uint8_t padding = INITIAL_PADDING;
+                               /* 2 LEDs as output, "switch" on SWITCH */
   LED_DDR = ((1 << LED0) | (1 << LED1) | (1 << SWITCH));
   initADC();
   initUSART();
 
   // ------ Event loop ------ //
   while (1) {
-		adcValue = readADC(PIEZO);                        
-    // Keep long-running moving average -- should approximate midpoint  
-		middleValue = ((adcValue + 63*middleValue + 32) >> 6);
-		
-		// Now check to see if ADC value above or below thresholds
-		if (adcValue < (middleValue - padding)) {
-      LED_PORT = (1 << LED0) | (1 << SWITCH);         /* one LED, switch */
-      lightsOutTime = ON_TIME/CYCLE_DELAY;          /* reset timer */
+    adcValue = readADC(PIEZO);
+    // Keep long-running moving average -- will average out to midpoint
+    middleValue = ((adcValue + 63 * middleValue + 32) >> 6);
+
+    // Now check to see if ADC value above or below thresholds
+    if (adcValue < (middleValue - padding)) {
+      LED_PORT = (1 << LED0) | (1 << SWITCH);       /* one LED, switch */
+      lightsOutTime = ON_TIME / CYCLE_DELAY;            /* reset timer */
     }
     else if (adcValue > (middleValue + padding)) {
-      LED_PORT = (1 << LED1) | (1 << SWITCH);       /* other LED, switch */
-      lightsOutTime = ON_TIME/CYCLE_DELAY;     /* reset timer */
+      LED_PORT = (1 << LED1) | (1 << SWITCH);     /* other LED, switch */
+      lightsOutTime = ON_TIME / CYCLE_DELAY;            /* reset timer */
     }
     else {                            /* Nothing seen, turn off lights */
       LED_PORT &= ~(1 << LED0);
       LED_PORT &= ~(1 << LED1);                            /* Both off */
-      if (lightsOutTime > 0) {  /* still have time left */
-				lightsOutTime--;  
+      if (lightsOutTime > 0) {                 /* still have time left */
+        lightsOutTime--;
       }
-			else{ /* time's up */ 
-        LED_PORT &= ~(1 << SWITCH);                   /* turn switch off */
-			}
+      else {                                              /* time's up */
+        LED_PORT &= ~(1 << SWITCH);                 /* turn switch off */
+      }
     }
-#if USE_POT  /* optional sensitivity potentiometer */ 
-		padding = readADC(POT) >> 4;  /* scale down to useful range */ 
+#if USE_POT                      /* optional sensitivity potentiometer */
+    padding = readADC(POT) >> 4;         /* scale down to useful range */
 #endif
-
+  
 		// Serial output and delay
-		transmitByte(adcValue >> 2);
-		_delay_ms(CYCLE_DELAY);
+    transmitByte(adcValue >> 2);
+    _delay_ms(CYCLE_DELAY);
   }                                                  /* End event loop */
   return (0);                            /* This line is never reached */
 }
