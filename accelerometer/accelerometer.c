83ba9928d682c95a7ec8c70b2b434145cc92c216
Elliot Williams <elliot@pre.postero.us>
2013-08-25 18:29:04 +0200

    done tweaking

--- accelerometer/accelerometer.c
+++ accelerometer/accelerometer.c
@@ -1,6 +1,5 @@
 /*
  * Maximally-sensitive footstep-detector.  
- * Uses moving averages to automatically separate signal from noise
  */
 
 // ------- Preamble -------- //
@@ -10,10 +9,17 @@
 #include "pinDefines.h"
 #include "USART.h"
 
-#define ON_TIME             2000             /* milliseconds */
-#define CYCLE_DELAY           10             /* milliseconds */
+#define ON_TIME            2000             /* milliseconds */
+#define CYCLE_DELAY           9             /* milliseconds */
+#define INITIAL_PADDING      10
 
 #define SWITCH              PB7 /* LED or light-switch.  It's all in the hookup. */ 
+
+#define USE_POT               0 /* define to 1 if using potentiometer */ 
+#if USE_POT
+  #define POT               PC5  /* optional sensitivity pot on PC5 */ 
+#endif
+
 // -------- Functions --------- //
 void initADC(void) {
   ADMUX  |= (1 << REFS0);                 /* reference voltage to AVCC */
@@ -32,10 +38,8 @@ int main(void) {
   // -------- Inits --------- //
   uint16_t lightsOutTime=0;                      /* timer for the switch */
 	uint16_t adcValue;
-	/*uint16_t maxValue=0;                      [> upper activity threshold <]*/
-	/*uint16_t minValue=1023;                      [> lower activity threshold <]*/
 	uint16_t middleValue=511;  /* average value */ 
-	uint16_t padding = 6;
+	uint8_t padding = INITIAL_PADDING;
 
 	/* 2 LEDs as output, "switch" on SWITCH */
   LED_DDR = ((1 << LED0) | (1 << LED1) | (1 << SWITCH));
@@ -48,46 +52,31 @@ int main(void) {
     // Keep long-running moving average -- should approximate midpoint  
 		middleValue = ((adcValue + 63*middleValue + 32) >> 6);
 		
-		// Update the thresholds  
-		if (adcValue > middleValue){
-			/*maxValue = ((adcValue + 63*maxValue + 32) >> 6);*/
-			padding = ((adcValue - middleValue) +  3*padding + 2) >> 2;
-		}
-		else if (adcValue < middleValue){
-			/*minValue = ((adcValue + 63*minValue + 32) >> 6);*/
-			padding = ((middleValue - adcValue) +  3*padding + 2) >> 2;
-		}
-	 /*else{	*/
-			/*maxValue = ((adcValue + 31*maxValue + 15) >> 5) ;*/
-			/*minValue = ((adcValue + 31*minValue + 15) >> 5) ;*/
-		/*}*/
 		// Now check to see if ADC value above or below thresholds
-		if (adcValue < (middleValue - 2*padding)) {
+		if (adcValue < (middleValue - padding)) {
       LED_PORT = (1 << LED0) | (1 << SWITCH);         /* one LED, switch */
       lightsOutTime = ON_TIME/CYCLE_DELAY;          /* reset timer */
     }
-    else if (adcValue > (middleValue + 2*padding)) {
+    else if (adcValue > (middleValue + padding)) {
       LED_PORT = (1 << LED1) | (1 << SWITCH);       /* other LED, switch */
       lightsOutTime = ON_TIME/CYCLE_DELAY;     /* reset timer */
     }
     else {                            /* Nothing seen, turn off lights */
       LED_PORT &= ~(1 << LED0);
       LED_PORT &= ~(1 << LED1);                            /* Both off */
-			/*maxValue = ((adcValue + 31*maxValue + 15) >> 5) ;*/
-			/*minValue = ((adcValue + 31*minValue + 15) >> 5) ;*/
-      if (lightsOutTime > 0) {  /* if no activity */
-				lightsOutTime--; /* And decrement lights-out timer */ 
+      if (lightsOutTime > 0) {  /* still have time left */
+				lightsOutTime--;  
       }
-			else{
+			else{ /* time's up */ 
         LED_PORT &= ~(1 << SWITCH);                   /* turn switch off */
 			}
     }
+#if USE_POT  /* optional sensitivity potentiometer */ 
+		padding = readADC(POT) >> 4;  /* scale down to useful range */ 
+#endif
 
 		// Serial output and delay
-		/*transmitByte(adcValue >> 2);*/
-		/*transmitByte(minValue >> 2); */
-		/*transmitByte(maxValue >> 2);*/
-		transmitByte(padding);
+		transmitByte(adcValue >> 2);
 		_delay_ms(CYCLE_DELAY);
   }                                                  /* End event loop */
   return (0);                            /* This line is never reached */
