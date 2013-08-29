8b5e72426c11f6ebb47808fd78b6cd8b734a6365
Elliot Williams <elliot@pre.postero.us>
2013-08-29 15:54:38 +0200

    cosmetic tweakery.  final version?

--- footstepDetector/footstepDetector.c
+++ footstepDetector/footstepDetector.c
@@ -1,5 +1,5 @@
 /*
- *      Sensitive footstep-detector.
+ *      Sensitive footstep-detector and EWMA demo
  */
 
 // ------- Preamble -------- //
@@ -11,18 +11,18 @@
 
 #define ON_TIME            2000                        /* milliseconds */
 #define CYCLE_DELAY          10                        /* milliseconds */
-#define INITIAL_SENSITIVITY  10
+#define INITIAL_SENSITIVITY  10            /* higher is less sensitive */ 
 
-#define SWITCH              PB7
+#define SWITCH              PB7     /* Attach LED or switch relay here */ 
 
-#define USE_POT           0  /* define to 1 if using potentiometer */
+#define USE_POT               0  /* define to 1 if using potentiometer */
 #if USE_POT
-  #define POT           PC5     /* optional sensitivity pot on PC5 */
+  #define POT               PC5            /* optional sensitivity pot */
 #endif
 
 // -------- Functions --------- //
 void initADC(void) {
-  ADMUX |= (1 << REFS0);                  /* reference voltage to AVCC */
+  ADMUX  |= (1 << REFS0);                  /* reference voltage to AVCC */
   ADCSRA |= (1 << ADPS1) | (1 << ADPS2);    /* ADC clock prescaler /64 */
   ADCSRA |= (1 << ADEN);                                 /* enable ADC */
 }
@@ -41,10 +41,10 @@ int main(void) {
   uint16_t middleValue = 511;
   uint16_t highValue = 1023;
   uint16_t lowValue = 0;
-	uint16_t noiseVolume = 0;
-	uint8_t sensitivity = INITIAL_SENSITIVITY;
-                               /* 2 LEDs as output, "switch" on SWITCH */
-  LED_DDR = ((1 << LED0) | (1 << LED1) | (1 << SWITCH));
+  uint16_t noiseVolume = 0;
+  uint8_t  sensitivity = INITIAL_SENSITIVITY;
+  
+	LED_DDR = ((1 << LED0) | (1 << LED1) | (1 << SWITCH));
   initADC();
   initUSART();
 
@@ -52,25 +52,25 @@ int main(void) {
   while (1) {
     adcValue = readADC(PIEZO);
 
-   /* Keep long-running moving average -- will average out to midpoint */
-		middleValue = adcValue + middleValue - ((middleValue - 8) >> 4) ; 
-		// The "sensitivity" variable keeps the high and low bounds away
-    // from the middle value, which desensitizes the sensor.
+        /* Long-running moving average -- tracks sensor's bias voltage */
+    middleValue = adcValue + middleValue - ((middleValue - 8) >> 4);
+    // The "sensitivity" variable keeps the high and low bounds away
+		//       from the middle value, desensitizing the sensor.
     if (adcValue > ((middleValue >> 4) + sensitivity)) {
-      highValue = adcValue + highValue - ((highValue - 8) >> 4) ;
+      highValue = adcValue + highValue - ((highValue - 8) >> 4);
     }
     if (adcValue < ((middleValue >> 4) - sensitivity)) {
-      lowValue = adcValue + lowValue - ((lowValue - 8) >> 4) ;
+      lowValue = adcValue + lowValue - ((lowValue - 8) >> 4);
     }
-		noiseVolume = highValue - lowValue;
+    noiseVolume = highValue - lowValue;
 
             /* Now check to see if ADC value above or below thresholds */
-	 /* Comparison with << 4 b/c the other values come from EWMA */ 
-		if ( ( adcValue<<4) < (middleValue - noiseVolume)) {
+                /* Comparison with >> 4 b/c EWMA is on different scale */
+    if (adcValue < ((middleValue - noiseVolume) >> 4)) {
       LED_PORT = (1 << LED0) | (1 << SWITCH);       /* one LED, switch */
       lightsOutTimer = ON_TIME / CYCLE_DELAY;           /* reset timer */
     }
-    else if ((adcValue<<4) > (middleValue + noiseVolume)) {
+    else if (adcValue > ((middleValue + noiseVolume) >> 4)) {
       LED_PORT = (1 << LED1) | (1 << SWITCH);     /* other LED, switch */
       lightsOutTimer = ON_TIME / CYCLE_DELAY;           /* reset timer */
     }
@@ -89,13 +89,8 @@ int main(void) {
 #endif
 
     // Serial output and delay
-		/* ADC is 10-bits, here recenter around 127  */ 
-		transmitByte(adcValue  - 512 + 127);
-		/*transmitByte((adcValue << 4) - middleValue);*/
-		/*transmitByte(noiseVolume >> 4);*/
-		/*transmitByte((middleValue >> 4) - 512 + 127);*/
-		/*transmitByte((lowValue >> 4) - 512 + 127);*/
-		/*transmitByte((highValue >> 4) - 512 + 127);*/
+           /* ADC is 10-bits, recenter around 127 for display purposes */
+    transmitByte(adcValue - 512 + 127);
     _delay_ms(CYCLE_DELAY);
   }                                                  /* End event loop */
   return (0);                            /* This line is never reached */
