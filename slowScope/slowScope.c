c6ddd0394099e31e5fcbf6c4a168fe456a366d7d
Elliot Williams <elliot@pre.postero.us>
2013-09-16 18:59:03 +0200

    reworked slowscope really quick to remove interrupts

--- slowScope/slowScope.c
+++ slowScope/slowScope.c
@@ -1,35 +1,31 @@
-               /* Slow-scope.  A free-running AVR / ADC "oscilloscope" */
+// Slow-scope.  A free-running AVR / ADC "oscilloscope"
 
 // ------- Preamble -------- //
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
 #include "pinDefines.h"
 #include "USART.h"
 
+#define SAMPLE_DELAY  20 /* ms, controls the scroll-speed of the scope */ 
+
 // -------- Functions --------- //
-static inline void initADC(void) {
+static inline void initFreerunningADC(void) {
   ADMUX |= (1 << REFS0);                  /* reference voltage on AVCC */
   ADMUX |= (1 << ADLAR);     /* left-adjust result, return only 8 bits */
   ADCSRA |= (1 << ADPS2) | (1 << ADPS0);    /* ADC clock prescaler /32 */
   ADCSRA |= (1 << ADATE);                       /* auto-trigger enable */
-  ADCSRA |= (1 << ADIE);             /* enable interrupt when complete */
   ADCSRA |= (1 << ADEN);                                 /* enable ADC */
   ADCSRA |= (1 << ADSC);                     /* start first conversion */
   sei();
 }
 
-ISR(ADC_vect) {
-  transmitByte(ADCH);    /* just transmit the high byte, left-adjusted */
-}
-
 int main(void) {
   // -------- Inits --------- //
-  initUSART();
-  initADC();
+  initFreerunningADC();
   // ------ Event loop ------ //
   while (1) {
-    ;                      /* Nothing. It's all done in the interrupt. */
+    transmitByte(ADCH);       /* transmit the high byte, left-adjusted */
+    _delay_ms(SAMPLE_DELAY);
   }                                                  /* End event loop */
   return (0);                            /* This line is never reached */
 }
