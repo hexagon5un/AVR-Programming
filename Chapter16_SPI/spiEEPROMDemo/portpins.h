318b6af0a395971149cde78279c535907c35f0a1
Elliot Williams <elliot@pre.postero.us>
2014-01-26 23:41:37 +0100

    killed one more irrelevant file

--- Chapter16_SPI/spiEEPROMDemo/portpins.h
+++
@@ -1,5 +0,0 @@
-/* Copyright (c) 2003  Theodore A. Roth
-   All rights reserved.
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are met:
