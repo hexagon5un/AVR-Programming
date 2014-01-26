31f3c0831ba3b69a31778e071d24e9834e6b6613
Elliot Williams <elliot@pre.postero.us>
2014-01-26 17:24:14 +0100

    CH03: cleaning out the old POV routines

--- Chapter03_Digital-Output/povToy/bigFont.h
+++
@@ -1,1 +0,0 @@
-// From http://rucalgary.hackhut.com/2011/04/26/upov-with-better-firmware-pics-video-and-source/
--- Chapter03_Digital-Output/povToy/invaders.c
+++ Chapter03_Digital-Output/povToy/invaders.c
@@ -1,21 +1,15 @@
 /*
   Space Invaders POV demo by Elliot Williams
+  This version stores the displays in arrays,
+  loops over their elements in a function.
 */
 
 #include <avr/io.h>
 #include <util/delay.h>
 #define DELAYTIME 2                                              /* ms */
-#include "povRoutines.h"
-
-#define BUTTON_DDR    DDRD                            /* button on PD4 */
-#define BUTTON_INPUT  PIND
-#define BUTTON_PORT   PORTD
-#define BUTTON        PD2
 
 #define LED_PORT                PORTB
-#define LED_PIN                 PINB
 #define LED_DDR                 DDRB
-
 
 uint8_t invaderData1[] = {
   0b01110000,
@@ -46,33 +40,30 @@ uint8_t invaderData2[] = {
 };
 
 
-
-void init(void) {
-  // Set up all of bank B pins for output
-  LED_DDR = 0xff;
-
-  // Init button input, with pullup resistor
-                       /* not necessary, but double-sure in input mode */
-  BUTTON_DDR &= ~(1 << BUTTON);
-  BUTTON_PORT |= (1 << BUTTON);                     /* activate pullup */
+void pause(void){
+	uint8_t i;
+	for (i=0; i<5; i++){
+		_delay_ms(DELAYTIME);
+	}
 }
 
+void POVDisplay(uint8_t povData[], uint8_t numberRows) {
+  uint8_t i;
+  for (i = 0; i < numberRows; ++i) {
+    LED_PORT = povData[i];
+    _delay_ms(DELAYTIME);
+  }
+  pause();
+}
 
 int main(void) {
 
-  init();
+  LED_DDR = 0xff;                            /* set all LEDs to output */ 
 
   while (1) {                                              /* mainloop */
 
-    if (bit_is_set(BUTTON_INPUT, BUTTON)) {
       POVDisplay(invaderData1, sizeof(invaderData1));
-      pause();
-    }
-
-    else {
       POVDisplay(invaderData2, sizeof(invaderData2));
-      pause();
-    }
 
   }                                                    /* end mainloop */
   return (0);
